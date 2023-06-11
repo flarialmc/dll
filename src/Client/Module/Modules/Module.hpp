b6c868d88234001054825c6c87c3def5b4ed3976
Bari <58800830+TheBarii@users.noreply.github.com>
2023-06-11 16:02:59 +0600

    config system (real)

--- src/Client/Module/Modules/Module.hpp
+++ src/Client/Module/Modules/Module.hpp
@@ -2,6 +2,9 @@
 
 #include "../../../Utils/Utils.hpp"
 #include <string>
+#include "../../../Config/Settings.hpp"
+#include <fstream>
+#include <iostream>
 
 class Module
 {
@@ -9,6 +12,9 @@ public:
     std::string name;
     std::string description;
     std::string icon;
+    Settings settings;
+    std::string settingspath;
+
     int keybind;
 
     Module(const std::string& ename, const std::string& edescription, const std::string& eicon, int ekey) {
@@ -16,13 +22,39 @@ public:
         description = edescription;
         icon = eicon;
         keybind = ekey;
+        settings = Settings();
+        settingspath = Utils::getRoamingPath() + "\\Config\\" + name + ".flarial";
     }
 public:
     bool enabled = false;
 
 public:
 
-    virtual void onEnable() {}
+void SaveSettings() {
+    try {
+        // Open the file for writing
+        std::ofstream outputFile(settingspath);
 
-    virtual void onDisable() {}
+
+        json jsonData = settings.ToJson();
+        if (outputFile.is_open()) {
+            // Write the JSON to the file
+            outputFile << settings.ToJson();  // Indent with 4 spaces for readability
+            outputFile.close();
+        } else {
+            std::cerr << "Failed to open file: " << settingsPath << std::endl;
+        }
+    } catch (const std::exception& ex) {
+        std::cerr << "Exception occurred while saving settings: " << ex.what() << std::endl;
+    }
+}
+
+
+virtual void onEnable() {}
+
+    virtual void onDisable() {
+
+        SaveSettings();
+
+   }
 };
---
+++ src/Config/Settings.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <unordered_map>
+#include "json/json.hpp"
+#include "../Utils/Logger/Logger.hpp"
+
+using json = nlohmann::json;
+
+// Forward declaration
+class Setting;
+
+// Customizable Setting type
+template<typename T>
+class SettingType {
+public:
+    SettingType(const std::string& name, const T& defaultValue) : name(name), value(defaultValue) {}
+
+    std::string name;
+    T value;
+};
+
+// Base Setting class
+class Setting {
+public:
+    virtual ~Setting() = default;
+    virtual json ToJson() const = 0;
+    virtual void FromJson(const json& jsonData) = 0;
+};
+
+// Template specialization for SettingType<T>
+template<typename T>
+class TypedSetting : public Setting {
+public:
+    TypedSetting(const std::string& name, const T& defaultValue) : setting(name, defaultValue) {}
+
+    json ToJson() const override {
+        json jsonData;
+        jsonData["name"] = setting.name;
+        jsonData["value"] = setting.value;
+        return jsonData;
+    }
+
+    void FromJson(const json& jsonData) override {
+        if (jsonData.is_object() && jsonData.contains("name") && jsonData.contains("value")) {
+            setting.name = jsonData["name"].get<std::string>();
+            setting.value = jsonData["value"].get<T>();
+        }
+    }
+
+    SettingType<T> setting;
+};
+
+// Settings container
+class Settings {
+public:
+    template<typename T>
+    void addSetting(const std::string& name, const T& defaultValue) {
+        settings.emplace_back(new TypedSetting<T>(name, defaultValue));
+    }
+
+    template<typename T>
+    SettingType<T>* getSettingByName(const std::string& name) {
+        for (auto& setting : settings) {
+            auto typedSetting = dynamic_cast<TypedSetting<T>*>(setting.get());
+            if (typedSetting && typedSetting->setting.name == name) {
+                return &(typedSetting->setting);
+            }
+        }
+        return nullptr;
+    }
+
+    template<typename T>
+    void setValue(const std::string& name, const T& value) {
+        auto setting = getSettingByName<T>(name);
+        if (setting) {
+            setting->value = value;
+        }
+    }
+
+    std::string ToJson() const {
+        json jsonData;
+        for (const auto& setting : settings) {
+            jsonData.push_back(setting->ToJson());
+        }
+        return jsonData.dump(4);
+    }
+
+    void FromJson(const std::string& jsonString) {
+        try {
+            json jsonData = json::parse(jsonString);
+            for (const auto& item : jsonData) {
+                std::string name = item["name"].get<std::string>();
+                for (auto& setting : settings) {
+                    if (setting->ToJson()["name"].get<std::string>() == name) {
+                        setting->FromJson(item);
+                        break;
+                    }
+                }
+            }
+        } catch (const std::exception& e) {
+            Logger::debug(e.what());
+        }
+    }
+
+private:
+    std::vector<std::unique_ptr<Setting>> settings;
+};
