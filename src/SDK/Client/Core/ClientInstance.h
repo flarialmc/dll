714cfc342b76d0e3b82c2e871b9117fb0f1853c2
White <ThinkUp@DESKTOP-37P627H>
2023-06-14 10:36:01 +0100

    soon

---
+++ src/SDK/Client/Core/ClientInstance.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../Actor/LocalPlayer.h"
+#include <cstdint>
+
+class ClientInstance {
+	uintptr_t** VTable;
+
+	LocalPlayer* getLocalPlayer() {
+		//here we call on getLocalPlayer through the virtual function table by getting its addr
+		//then casting it into a function signature LocalPlayer*(__fastcall*)(ClientInstance*))
+		auto getLocalPlayerFunc = reinterpret_cast<LocalPlayer*(__fastcall*)(ClientInstance*))> (this->VTable[27]);
+		return getLocalPlayerFunc(this);
+	}
+};
