ffa94ceedfc6cc92076bbe92dc36af960cc4b929
AnshulMaster <anshuldeep61@gmail.com>
2023-06-26 12:21:18 +0530

    text!!11!!1

--- src/Client/Module/Modules/CPS/CPSCounter.hpp
+++ src/Client/Module/Modules/CPS/CPSCounter.hpp
@@ -120,6 +120,8 @@ public:
         toggleY += Constraints::SpacingConstraint(0.30, textWidth);
         toggleX = Constraints::PercentageConstraint(0.019, "left");
 
+        FlarialGUI::FlarialTextWithFont(toggleX+ Constraints::SpacingConstraint(0.60, textWidth), toggleY, FlarialGUI::to_wide("Right CPS").c_str(), D2D1::ColorF(D2D1::ColorF::White), textWidth * 1.4f, textHeight, DWRITE_TEXT_ALIGNMENT_LEADING, Constraints::SpacingConstraint(1.05, textWidth));
+
         if (FlarialGUI::Toggle(2, toggleX, toggleY, D2D1::ColorF(255.0f / 255.0f, 35.0f / 255.0f, 58.0f / 255.0f), D2D1::ColorF(112.0f / 255.0f, 75.0f / 255.0f, 82.0f / 255.0f), D2D1::ColorF(D2D1::ColorF::White), this->settings.getSettingByName<bool>("rightcps")->value)) this->settings.getSettingByName<bool>("rightcps")->value = !this->settings.getSettingByName<bool>("rightcps")->value;
 
         /* Rounding End */
