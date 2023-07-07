0c3ab7d193a070627e6284caa7192d6f8757a485
Bari <58800830+TheBarii@users.noreply.github.com>
2023-07-08 02:45:29 +0600

    AAAAAAAAAAAAAA

--- src/Client/GUI/Engine/Engine.cpp
+++ src/Client/GUI/Engine/Engine.cpp
@@ -470,8 +470,6 @@ void FlarialGUI::TextBoxVisual(int index, std::string& text, int limit, float x,
 
     FlarialGUI::FlarialTextWithFont(x, y, to_wide(text).c_str(), D2D1::ColorF(D2D1::ColorF::White), Constraints::SpacingConstraint(1.55, textWidth), percHeight, DWRITE_TEXT_ALIGNMENT_CENTER, 110);
     FlarialGUI::FlarialTextWithFont(x + Constraints::SpacingConstraint(1.70, textWidth), y, to_wide("Text Format").c_str(), D2D1::ColorF(D2D1::ColorF::White), Constraints::SpacingConstraint(1.55, textWidth), percHeight, DWRITE_TEXT_ALIGNMENT_LEADING, 110);
-
-    return "";
 }
 
 
