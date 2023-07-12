6ee7fa97119b13ef216095b9355c3df1e6c471ea
indig0r <138825508+Indig0r@users.noreply.github.com>
2023-07-12 17:23:26 +0100

    public:

--- src/SDK/Client/Render/GuiData.hpp
+++ src/SDK/Client/Render/GuiData.hpp
@@ -4,6 +4,7 @@
 #include "../../../Utils/Utils.hpp"
 
 class GuiData {
+public:
 	BUILD_ACCESS(this, Vec2<float>, ScreenSize, 0x0030);
 	BUILD_ACCESS(this, Vec2<float>, ScreenSizeScaled, 0x0040);
 	BUILD_ACCESS(this, float, GuiScale, 0x004C);
