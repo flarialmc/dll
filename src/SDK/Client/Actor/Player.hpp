2406caf6f2f3d782b5a82a208690c094fee10333
Indig0r <138825508+Indig0r@users.noreply.github.com>
2023-07-28 14:53:28 +0100

    Correct offset

--- src/SDK/Client/Actor/Player.hpp
+++ src/SDK/Client/Actor/Player.hpp
@@ -9,5 +9,5 @@ class Player : public Mob {
 public:
 	BUILD_ACCESS(this, PlayerInventory*, playerInventory, 0x7C0) // Player::getSupplies return x 8
 	BUILD_ACCESS(this, std::string, playerName, 0x1C78);
-	BUILD_ACCESS(this, Gamemode*, gamemode, 0xEF8);
+	BUILD_ACCESS(this, Gamemode*, gamemode, 0xE70);
 };
