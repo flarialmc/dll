541298600764f5ecdfca50b3b99d53f9f31a1928
anshul master <anshuldeep61@gmail.com>
2023-08-17 17:34:45 +0530

    speed counter (back end)

--- src/Client/Module/Modules/ForceCoords/ForceCoordsListener.hpp
+++ src/Client/Module/Modules/ForceCoords/ForceCoordsListener.hpp
@@ -38,24 +38,15 @@ class ForceCoordsListener : public Listener {
             }
         }
     }
+    Vec3<float> PrevPoss;
+    void onLocalTick(TickEvent& event) override {
+        Vec3<float> Pos = SDK::clientInstance->getLocalPlayer()->stateVector->Pos;
+        float* timer = SDK::clientInstance->getMinecraft()->timer;
 
-//    void onLocalTick(TickEvent& event) override {
-//        Vec3<float> Pos = SDK::clientInstance->getLocalPlayer()->stateVector->Pos;
-//        Vec3<float> PrevPos = SDK::clientInstance->getLocalPlayer()->stateVector->PrevPos;
-//        Vec3<float> vel = SDK::clientInstance->getLocalPlayer()->stateVector->PrevPos;
-//        float* timer = SDK::clientInstance->getMinecraft()->timer;
-//
-//        std::string cords = std::to_string(static_cast<int>(Pos.x)) + ", " + std::to_string(static_cast<int>(Pos.y)) + ", " + std::to_string(static_cast<int>(Pos.z));
-//        std::string cords1 = std::to_string(static_cast<int>(PrevPos.x)) + ", " + std::to_string(static_cast<int>(PrevPos.y)) + ", " + std::to_string(static_cast<int>(PrevPos.z));
-//        std::string cords2 = std::to_string(static_cast<int>(vel.x)) + ", " + std::to_string(static_cast<int>(vel.y)) + ", " + std::to_string(static_cast<int>(vel.z));
-//
-//        float speed = Pos.dist(PrevPos);
-//
-//        std::cout << std::to_string(speed) << std::endl;
-//        std::cout << cords << std::endl;
-//        std::cout << cords2 << std::endl;
-//        std::cout << "" << std::endl;
-//    }
+        
+        std::cout << std::to_string(Pos.dist(PrevPoss) * 20) << std::endl;
+        PrevPoss = Pos;
+    }
 
 public:
     explicit ForceCoordsListener(const char string[5], Module* module) {
