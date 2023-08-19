879f27bf3c7dcbc2e741b87fa12bdda5bf01decb
Melvin1663 <johnmelvinlabad800@gmail.com>
2023-08-19 21:25:52 +0800

    ok idk anymore someone fix this

--- src/Client/Module/Modules/Misc/CentreCursor/CentreCursorListener.hpp
+++ src/Client/Module/Modules/Misc/CentreCursor/CentreCursorListener.hpp
@@ -1,8 +1,16 @@
 #pragma once
+#include "../../../../Hook/Hooks/Input/MouseHook.hpp"
 #include "../../../../Events/Listener.hpp"
 #include "../../../../Client.hpp"
+
 class CentreCursorListener : public Listener {
 
+public:
+
+    void onLocalTick(TickEvent& event) override {
+        Client::centerCursor();
+    }
+    
 public:
     explicit CentreCursorListener(const char string[5]) {
         this->name = string;
