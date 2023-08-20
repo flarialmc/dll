63ab24e720ca25e821a8101c8a60a881dad8122b
Bari <58800830+TheBarii@users.noreply.github.com>
2023-08-20 17:53:38 +0600

    nick finished

--- src/Client/Module/Modules/Nick/NickListener.hpp
+++ src/Client/Module/Modules/Nick/NickListener.hpp
@@ -9,7 +9,33 @@
 
 class NickListener : public Listener {
     Module* module;
+    bool enabled = false;
+    std::string original;
 public:
+
+    void onLocalTick(TickEvent &event) override {
+
+        if(original.empty()) original = SDK::clientInstance->getLocalPlayer()->playerName;
+
+        if(enabled != module->settings.getSettingByName<bool>("enabled")->value) {
+            enabled = module->settings.getSettingByName<bool>("enabled")->value;
+
+            if(enabled) {
+
+                std::string val = module->settings.getSettingByName<std::string>("nick")->value;
+                SDK::clientInstance->getLocalPlayer()->setNametag(&val);
+                SDK::clientInstance->getLocalPlayer()->playerName = val;
+
+            } else {
+
+                std::string val = original;
+                SDK::clientInstance->getLocalPlayer()->setNametag(&val);
+                SDK::clientInstance->getLocalPlayer()->playerName =  val;
+
+            }
+        }
+    }
+
     explicit NickListener(const char string[5], Module* module) {
         this->name = string;
         this->module = module;
