acbbca9be64040dda7139257ec5b67499b2552b6
anshul master <anshuldeep61@gmail.com>
2023-08-24 21:21:46 +0530

    fixed small text bug

--- src/Client/Module/Modules/Animations/Animations.hpp
+++ src/Client/Module/Modules/Animations/Animations.hpp
@@ -42,10 +42,10 @@ public:
         const float textWidth = Constraints::RelativeConstraint(0.12, "height", true);
         const float textHeight = Constraints::RelativeConstraint(0.029, "height", true);
 
-        FlarialGUI::FlarialTextWithFont(toggleX, toggleY, L"HotBar Speed", D2D1::ColorF(D2D1::ColorF::White), textWidth, textHeight, DWRITE_TEXT_ALIGNMENT_LEADING, Constraints::RelativeConstraint(0.12, "height", true));
+        FlarialGUI::FlarialTextWithFont(toggleX, toggleY, L"HotBar", D2D1::ColorF(D2D1::ColorF::White), textWidth, textHeight, DWRITE_TEXT_ALIGNMENT_LEADING, Constraints::RelativeConstraint(0.12, "height", true));
 
 
-        float percent = FlarialGUI::Slider(1, toggleX + Constraints::SpacingConstraint(0.65, textWidth),
+        float percent = FlarialGUI::Slider(1, toggleX + Constraints::SpacingConstraint(0.60, textWidth),
             toggleY,
             D2D1::ColorF(255.0f / 255.0f, 36.0f / 255.0f, 56.0f / 255.0f),
             D2D1::ColorF(154.0f / 255.0f, 107.0f / 255.0f, 114.0f / 255.0f),
