6389019d8127e035da32126ada1f50d939dd4973
White <138825508+Indig0r@users.noreply.github.com>
2023-09-09 11:39:09 +0100

    Removal of unnecessary code.

--- src/SDK/Client/Render/MinecraftUIRenderContext.hpp
+++ src/SDK/Client/Render/MinecraftUIRenderContext.hpp
@@ -20,13 +20,4 @@ class MinecraftUIRenderContext {
 public:
 	BUILD_ACCESS(this, ClientInstance*, clientInstance, 0x8);
 	BUILD_ACCESS(this, ScreenContext*, screenContext, 0x10);
-	void drawRectangle(Vec4<float> const& rect, MCCColor const& colour, float alpha, int radius);
-	void fillRectangle(Vec4<float> const& rect, MCCColor const& colour, float alpha);
-	void setClippingRectangle(Vec4<float> const& pos);
-	void saveCurrentClippingRectangle();
-	void restoreSavedClippingRectangle();
-	void flushString(float delta);
-	__int64 flushImages(MCCColor& color, float opacity, class StringHasher& hashedString);
-	__int64 drawText(FontBitmap* font, const float* pos, TextHolder* text, const float* color, float alpha, unsigned int textAlignment, const TextMeasureData* textMeasureData, const CaretMeasureData* caretMeasureData);
-	float getMeasureData(void* font, TextHolder* str, float textSize, bool calcWidth);
 };
