492cd0fc2fb8f34da023d28ab01a1d62c414f0df
White <96546467+WhiteOnGitHub@users.noreply.github.com>
2023-09-23 18:37:02 +0100

    4 big guys

--- src/Client/Hook/Hooks/Visual/FogColorHook.hpp
+++ src/Client/Hook/Hooks/Visual/FogColorHook.hpp
@@ -15,7 +15,7 @@ private:
 	static float* FogColorCallback(void* a1, void* a2, void* a3, void* a4) {
 
 
-        auto* color = new MCCColor();
+        auto color = MCCColor();
 
 		if (ModuleManager::getModule("Fog Color") != nullptr ) {
             if (ModuleManager::getModule("Fog Color")->settings.getSettingByName<bool>("enabled")->value) {
@@ -24,11 +24,11 @@ private:
                 if (ModuleManager::getModule("Fog Color")->settings.getSettingByName<bool>("color_rgb")->value) color2 = FlarialGUI::rgbColor;
                 else color2 = FlarialGUI::HexToColorF(ModuleManager::getModule("Fog Color")->settings.getSettingByName<std::string>("color")->value);
 
-                color->r = color2.r;
-                color->g = color2.g;
-                color->b = color2.b;
-                color->a = ModuleManager::getModule("Fog Color")->settings.getSettingByName<float>("colorOpacity")->value;
-                return color->arr;
+                color.r = color2.r;
+                color.g = color2.g;
+                color.b = color2.b;
+                color.a = ModuleManager::getModule("Fog Color")->settings.getSettingByName<float>("colorOpacity")->value;
+                return color.arr;
 
             }
         }
@@ -41,7 +41,7 @@ public:
     typedef float*(__thiscall* FogColorOriginal)(void*, void*, void*, void*);
     static inline FogColorOriginal func_original = nullptr;
 
-    FogColorHook() : Hook("Fog Color Hook", "41 0F 10 08 48 8B C2") {}
+    FogColorHook() : Hook("Fog Color Hook", "41 0F 10 08 48 8B C2 0F 28 D3 F3 0F 59 1D ? ? ? ? F3 0F 59 15 ? ? ? ? F3 0F 58 1D ? ? ? ? 0F 11 0A F3 0F 58 15 ? ? ? ? F3 0F 59 5A ?") {}
 
     void enableHook() override {
 
