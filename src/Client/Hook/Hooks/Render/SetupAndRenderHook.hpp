ecdba3edf03e9732d7330b45c0b6269ee85fcf23
White <138825508+Indig0r@users.noreply.github.com>
2023-09-23 18:23:16 +0100

    wow

--- src/Client/Hook/Hooks/Render/SetupAndRenderHook.hpp
+++ src/Client/Hook/Hooks/Render/SetupAndRenderHook.hpp
@@ -178,7 +178,7 @@ public:
 
 	typedef void(__thiscall* drawTextOriginal)(MinecraftUIRenderContext*, void*, float*, std::string*, float*, float, unsigned int, const void*, const void*);
 	static inline  drawTextOriginal func_originalText = nullptr;
-	SetUpAndRenderHook() : Hook("SetupAndRender", "48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? ? ? 70 B8 ? ? 78 A8 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B F2 48 89 54 ? ? 4C") {
+	SetUpAndRenderHook() : Hook("SetupAndRender", "48 8B C4 48 89 58 18 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 B8 0F 29 78 A8 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B FA 48 89 54 24 ? 4C 8B E9 48 89 4C 24 ? E8 ? ? ? ? 49 8B 85 ? ? ? ? 49 89 85 ? ? ? ? FF 15 ? ? ? ? 48 8B D8 FF 15 ? ? ? ? 48 81 FB ? ? ? ? 75 06") {
 	}
 
 	void enableHook() override {
