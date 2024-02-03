d0955b44a02256e4f63b14390f2fe0514fab20a6
FreezeEngine <mr.springtrap@yandex.ru>
2024-02-03 18:09:26 +0300

    Added Engine.hpp

--- src/Client/GUI/Engine/Engine.hpp
+++ src/Client/GUI/Engine/Engine.hpp
@@ -6,6 +6,7 @@
 #include <stack>
 #include <map>
 #include <vector>
+#include <winrt/base.h>
 #include <chrono>
 #include <unordered_map>
 
@@ -69,7 +70,7 @@ struct ColorPicker
     bool movingHueX = false;
     float opacX = 0.0f;
     bool movingOpacX = false;
-    Vec2<float> shade = {-1,-1};
+    Vec2<float> shade = { -1,-1 };
     bool movingShade = false;
     bool isAt1 = false;
 };
@@ -177,36 +178,45 @@ namespace FlarialGUI
     float inline scrollposmodifier = 0.f;
     float inline barscrollpos = 0;
     float inline barscrollposmodifier = 10.0f;
+
     inline WindowRect WindowRects[1000];
     inline SliderRect SliderRects[2500];
     inline TextBoxStruct TextBoxes[1000];
     inline ColorPicker ColorPickers[2000];
     inline DropdownStruct DropDownMenus[2000];
     inline KeybindSelectorer KeybindSelectors[2000];
+
     inline std::string currentKeybind;
     std::vector<Notification> inline notifications;
     bool inline isInWindowRect = false;
 
     inline ID2D1Effect* blur = nullptr;
     extern std::unordered_map<std::string, ToolTipStruct> Tooltips;
-    extern std::unordered_map<std::string, ID2D1SolidColorBrush*> brushCache;
-    extern std::unordered_map<std::string, IDWriteTextFormat*> textFormatCache;
-    extern std::unordered_map<std::string, ID2D1GradientStopCollection*> gradientStopCache;
-    extern std::unordered_map<std::string, ID2D1LinearGradientBrush*> gradientBrushCache;
+    extern std::unordered_map<UINT32, winrt::com_ptr<ID2D1SolidColorBrush>> brushCache;
+    extern std::unordered_map<uint64_t, winrt::com_ptr<IDWriteTextLayout>> textLayoutCache;
+    extern std::unordered_map<uint64_t, winrt::com_ptr<IDWriteTextFormat>> textFormatCache;
+    extern std::unordered_map<std::string, winrt::com_ptr<ID2D1GradientStopCollection>> gradientStopCache;
+    extern std::unordered_map<std::string, winrt::com_ptr<ID2D1LinearGradientBrush>> gradientBrushCache;
 
     void PushSize(float x, float y, float width, float height);
     void PopSize();
     void PopAllStack();
-    bool Button(const float x, float y, const D2D_COLOR_F color, const D2D_COLOR_F textColor, const wchar_t *text, const float width = 100.0f, const float height = 160.0f);
-    bool RoundedButton(const int index, const float x, float y, const D2D_COLOR_F color, const D2D_COLOR_F textColor, const wchar_t *text, const float width = 160.0f, const float height = 100.0f, const float radiusX = 10.0f, const float radiusY = 10.0f, bool glow = false);
+    IDWriteTextFormat* GetTextFormat(const DWRITE_TEXT_ALIGNMENT alignment, DWRITE_PARAGRAPH_ALIGNMENT paragraphAlignment, const float fontSize,
+        const DWRITE_FONT_WEIGHT weight, bool moduleFont);
+    IDWriteTextLayout* GetTextLayout(const wchar_t* text,
+        const DWRITE_TEXT_ALIGNMENT alignment = DWRITE_TEXT_ALIGNMENT_CENTER, DWRITE_PARAGRAPH_ALIGNMENT paragraphAlignment = DWRITE_PARAGRAPH_ALIGNMENT_CENTER, const float fontSize = 14,
+        const DWRITE_FONT_WEIGHT weight = DWRITE_FONT_WEIGHT_NORMAL, float maxWidth = 500, float maxHeight = 500, bool moduleFont = false);
+    bool Button(const float x, float y, const D2D_COLOR_F color, const D2D_COLOR_F textColor, const wchar_t* text, const float width = 100.0f, const float height = 160.0f);
+    bool RoundedButton(const int index, const float x, float y, const D2D_COLOR_F color, const D2D_COLOR_F textColor, const wchar_t* text, const float width = 160.0f, const float height = 100.0f, const float radiusX = 10.0f, const float radiusY = 10.0f, bool glow = false);
     void RoundedRectOnlyTopCorner(const float x, float y, D2D_COLOR_F color, const float width = 160, const float height = 100, const float radiusX = 10, const float radiusY = 10);
     void RoundedRect(const float x, float y, const D2D_COLOR_F color, float width = 160.0f, const float height = 75, const float radiusX = 10.0f, const float radiusY = 10.0f);
-    void LoadImageFromFile(const wchar_t *filename, ID2D1Bitmap **bitmap);
-    void RoundedRectWithImageAndText(int index, const float x, float y, const float width, const float height, const D2D1_COLOR_F color, const std::string imagePath, const float imageWidth, const float imageHeight, const wchar_t *text);
-    std::wstring to_wide(const std::string &multi);
+    void LoadImageFromFile(const wchar_t* filename, ID2D1Bitmap** bitmap);
+    void RoundedRectWithImageAndText(int index, const float x, float y, const float width, const float height, const D2D1_COLOR_F color, const std::string imagePath, const float imageWidth, const float imageHeight, const wchar_t* text);
+    //std::wstring to_wide(const std::string& str);
+    //std::string from_wide(const std::wstring& wstr);
     void Image(const std::string imageName, const D2D1_RECT_F rect);
-    void FlarialText(float x, float y, const wchar_t *text, float width, const float height,
-                     const DWRITE_TEXT_ALIGNMENT alignment);
+    void FlarialText(float x, float y, const wchar_t* text, float width, const float height,
+        const DWRITE_TEXT_ALIGNMENT alignment);
     void SetScrollView(float x, float y, float width, float height);
     void UnsetScrollView();
     Vec2<float> GetCenterXY(float rectWidth = 160.0f, float rectHeight = 75.0f);
@@ -217,8 +227,8 @@ namespace FlarialGUI
     Vec2<float> CalculateResizedXY(float x, float y, float width, float height);
     void UpdateWindowRects();
 
-    bool RoundedRadioButton(int index, float x, float y, const D2D_COLOR_F color, const D2D_COLOR_F textColor, const wchar_t *text,
-                            const float width, const float height, float radiusX, float radiusY, const std::string& radioName, const std::string& currentRadio);
+    bool RoundedRadioButton(int index, float x, float y, const D2D_COLOR_F color, const D2D_COLOR_F textColor, const wchar_t* text,
+        const float width, const float height, float radiusX, float radiusY, const std::string& radioName, const std::string& currentRadio);
 
     void ApplyGaussianBlur(float blurIntensity);
 
@@ -229,18 +239,18 @@ namespace FlarialGUI
     void ApplyCombinedDeepFry();
 
 
-    void CopyBitmap(ID2D1Bitmap1 *from, ID2D1Bitmap **to);
+    void CopyBitmap(ID2D1Bitmap1* from, ID2D1Bitmap** to);
 
     bool Toggle(int index, float x, float y, bool isEnabled);
     bool Toggle(int index, float x, float y, bool isEnabled, bool rgb);
 
     float Slider(int index, float x, float y, float startingPoint = 50.0f, float maxValue = 100.0f, const float minValue = 0.0f,
-                 bool zerosafe = true);
+        bool zerosafe = true);
 
-    std::string Dropdown(int index, float x, float y, const std::vector<std::string> options, std::string &value,
-                         std::string label);
+    std::string Dropdown(int index, float x, float y, const std::vector<std::string> options, std::string& value,
+        std::string label);
 
-    void Circle(float x, float y, const D2D1_COLOR_F &color, float radius);
+    void Circle(float x, float y, const D2D1_COLOR_F& color, float radius);
 
     std::string TextBox(int index, std::string text, int limit, float x, float y, float width, float height);
 
@@ -250,50 +260,52 @@ namespace FlarialGUI
 
     HSV RGBtoHSV(D2D1_COLOR_F rgb);
 
-    void HSLToRGB(float h, float s, float l, float &r, float &g, float &b);
+    void HSLToRGB(float h, float s, float l, float& r, float& g, float& b);
 
     bool CursorInRect(float rectX, float rectY, float width, float height);
 
-    std::string ColorFToHex(const D2D1_COLOR_F &color);
+    std::string ColorFToHex(const D2D1_COLOR_F& color);
 
     void
-    RoundedHollowRect(float x, float y, float borderWidth, const D2D_COLOR_F color, const float width, const float height, float radiusX,
-                      float radiusY);
+        RoundedHollowRect(float x, float y, float borderWidth, const D2D_COLOR_F color, const float width, const float height, float radiusX,
+            float radiusY);
 
     template <typename T>
-    void lerp(T &a, const T &b, float t);
+    void lerp(T& a, const T& b, float t);
 
     D2D_COLOR_F LerpColor(D2D_COLOR_F color1, D2D_COLOR_F color2, float percentage);
     D2D1::ColorF HSVtoColorF(float H, float s, float v);
 
-    void ColorPicker(int index, float x, float y, std::string &hex, float &opacity, bool& rgb);
+    void ColorPicker(int index, float x, float y, std::string& hex, float& opacity, bool& rgb);
 
     void ColorPickerWindow(int index, std::string& hex, float& opacity, bool& rgb);
 
-    D2D1::ColorF HexToColorF(const std::string &hexString);
+    D2D1::ColorF HexToColorF(const std::string& hexString);
+
+    std::wstring to_wide(const std::string& str);
 
     void
-    FlarialTextWithFont(float x, float y, const wchar_t *text, const float width, const float height,
-                        const DWRITE_TEXT_ALIGNMENT alignment, const float fontSize,
-                        const DWRITE_FONT_WEIGHT weight, bool moduleFont = false);
+        FlarialTextWithFont(float x, float y, const wchar_t* text, const float width, const float height,
+            const DWRITE_TEXT_ALIGNMENT alignment, const float fontSize,
+            const DWRITE_FONT_WEIGHT weight, bool moduleFont = false);
 
     void
-    FlarialTextWithFont(float x, float y, const wchar_t *text, const float width, const float height,
-                        const DWRITE_TEXT_ALIGNMENT alignment, const float fontSize,
-                        const DWRITE_FONT_WEIGHT weight, D2D1_COLOR_F color, bool moduleFont = false);
+        FlarialTextWithFont(float x, float y, const wchar_t* text, const float width, const float height,
+            const DWRITE_TEXT_ALIGNMENT alignment, const float fontSize,
+            const DWRITE_FONT_WEIGHT weight, D2D1_COLOR_F color, bool moduleFont = false);
 
     void ResetShit();
 
-    void AddShadowRect(const D2D1_POINT_2F &obj_min, const D2D1_POINT_2F &obj_max, D2D1_COLOR_F shadow_col,
-                       float shadow_thickness, const D2D1_POINT_2F &shadow_offset, float obj_rounding);
+    void AddShadowRect(const D2D1_POINT_2F& obj_min, const D2D1_POINT_2F& obj_max, D2D1_COLOR_F shadow_col,
+        float shadow_thickness, const D2D1_POINT_2F& shadow_offset, float obj_rounding);
 
-    void KeybindSelector(const int index, float x, float y, std::string &keybind);
+    void KeybindSelector(const int index, float x, float y, std::string& keybind);
 
     void Notify(std::string text);
 
     void BlurRect(D2D1_ROUNDED_RECT rect, float intensity);
 
-    void ApplyGaussianBlurToTarget(ID2D1BitmapRenderTarget *target, float blurIntensity);
+    void ApplyGaussianBlurToTarget(ID2D1BitmapRenderTarget* target, float blurIntensity);
 
     void NotifyHeartbeat();
 
@@ -301,12 +313,12 @@ namespace FlarialGUI
 
     ID2D1SolidColorBrush* getBrush(D2D1_COLOR_F color);
 
-    extern IDWriteFactory *writeFactory;
+    extern IDWriteFactory* writeFactory;
 
     extern ID2D1ImageBrush* blurbrush;
     extern ID2D1Factory* factory;
     extern ID2D1ImageBrush* shadowbrush;
-    extern std::unordered_map<std::string, ID2D1Image*> cachedBitmaps;
+    extern std::unordered_map<std::string, winrt::com_ptr<ID2D1Image>> cachedBitmaps;
 
     void ShadowRect(D2D1_ROUNDED_RECT rect, D2D1_COLOR_F color = D2D1::ColorF(0, 0, 0, 0.75f));
 
@@ -316,10 +328,7 @@ namespace FlarialGUI
 
     void InnerShadowRect(D2D1_ROUNDED_RECT rect, float howbig, D2D1_COLOR_F color = D2D1::ColorF(0, 0, 0, 0.75f));
 
-    bool isRectInRect(const D2D1_RECT_F &outer, const D2D1_RECT_F &inner);
-
-    IDWriteTextFormat *getTextFormat(const std::string &fontFamily, FLOAT fontSize, DWRITE_FONT_WEIGHT fontWeight,
-                                     DWRITE_FONT_STYLE fontStyle, DWRITE_FONT_STRETCH fontStretch, DWRITE_TEXT_ALIGNMENT alignment);
+    bool isRectInRect(const D2D1_RECT_F& outer, const D2D1_RECT_F& inner);
 
     void SetIsInAdditionalYMode();
     void UnSetIsInAdditionalYMode();
@@ -328,8 +337,8 @@ namespace FlarialGUI
     D2D1_COLOR_F getGradientColor(D2D1_GRADIENT_STOP gradientStops[10], float position);
     void RGBController();
 
-    ID2D1LinearGradientBrush *getLinearGradientBrush(float x, float hexPreviewSize, float shadePickerWidth,
-                                                     ID2D1GradientStopCollection *pGradientStops, std::string susKey);
+    ID2D1LinearGradientBrush* getLinearGradientBrush(float x, float hexPreviewSize, float shadePickerWidth,
+        ID2D1GradientStopCollection* pGradientStops, std::string susKey);
 
     void Tooltip(std::string id, float x, float y, std::string text, float width, float height, bool push = true, bool relative = false);
     void displayToolTips();
