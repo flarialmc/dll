6ce6494db6a28cc8164c74431302a472ca57b013
marioCST <68396929+marioCST@users.noreply.github.com>
2024-02-07 20:55:39 +0100

    fix getBreakprogress sig (thx FreezeEngine)

--- src/SDK/Client/Actor/Player.cpp
+++ src/SDK/Client/Actor/Player.cpp
@@ -3,8 +3,8 @@
 float Player::getBreakprogress(Block block) {
     static uintptr_t sig;
 
-    if (sig == NULL) { // Dead
-        sig = Memory::findSig("48 89 5C 24 08 57 48 83 EC 50 48 8B F9 0F 29 7C 24 30");
+    if (sig == NULL) {
+        sig = Memory::findSig("48 89 5C 24 08 57 48 83 EC 50 48 8B ? ? ? ? ? 30");
     }
 
     auto fn = reinterpret_cast<float (__thiscall*)(Player*, Block)>(sig);
