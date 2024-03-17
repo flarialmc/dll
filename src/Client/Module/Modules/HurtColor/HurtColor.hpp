6d7026a5343e73d9fde2caeef2c8ad57128dcb8a
FreezeEngine <mr.springtrap@yandex.ru>
2024-03-17 15:45:06 +0300

    Other modules: Using new events

--- src/Client/Module/Modules/HurtColor/HurtColor.hpp
+++ src/Client/Module/Modules/HurtColor/HurtColor.hpp
@@ -2,38 +2,37 @@
 
 #include "../Module.hpp"
 #include "../../../Events/EventHandler.hpp"
+#include "HurtColorListener.hpp"
 
 
 class HurtColor : public Module {
 
 public:
 
-    HurtColor() : Module("Hurt Color", "Change the color when you hit entities.", "\\Flarial\\assets\\hurt.png", 'C') {
+    HurtColor() : Module("Hurt Color", "Change the color when you hit entities.", R"(\Flarial\assets\hurt.png)", "") {
 
-        onEnable();
+        Module::setup();
 
     };
 
     void onEnable() override {
+        EventHandler::registerListener(new HurtColorListener("HurtColor", this));
         Module::onEnable();
-
     }
 
-    void NormalRender(int index, std::string text, std::string value) override {
-
+    void onDisable() override {
+        EventHandler::unregisterListener("HurtColor");
+        Module::onDisable();
     }
 
-    virtual void DefaultConfig() override {
-        if (settings.getSettingByName<std::string>("color") == nullptr) settings.addSetting("color", (std::string)"FFFFFF");
+    void defaultConfig() override {
+        if (settings.getSettingByName<std::string>("color") == nullptr)
+            settings.addSetting("color", (std::string) "FFFFFF");
         if (settings.getSettingByName<bool>("color_rgb") == nullptr) settings.addSetting("color_rgb", false);
         if (settings.getSettingByName<float>("colorOpacity") == nullptr) settings.addSetting("colorOpacity", 0.65);
     }
 
-    void onDisable() override {
-        Module::onDisable();
-    }
-
-    void SettingsRender() override {
+    void settingsRender() override {
 
         float textWidth = Constraints::RelativeConstraint(0.12, "height", true);
         const float textHeight = Constraints::RelativeConstraint(0.029, "height", true);
@@ -41,12 +40,18 @@ public:
         float x = Constraints::PercentageConstraint(0.019, "left");
         float y = Constraints::PercentageConstraint(0.10, "top");
 
-        FlarialGUI::FlarialTextWithFont(x, y, FlarialGUI::to_wide("Color").c_str(), textWidth * 6.9f, textHeight,
+        FlarialGUI::FlarialTextWithFont(x, y, L"Color", textWidth * 6.9f, textHeight,
                                         DWRITE_TEXT_ALIGNMENT_LEADING, Constraints::SpacingConstraint(1.05, textWidth),
                                         DWRITE_FONT_WEIGHT_NORMAL);
-        FlarialGUI::ColorPicker(0, x + FlarialGUI::SettingsTextWidth("Color "), y - Constraints::SpacingConstraint(0.017, textWidth), settings.getSettingByName<std::string>("color")->value, settings.getSettingByName<float>("colorOpacity")->value, settings.getSettingByName<bool>("color_rgb")->value);
+        FlarialGUI::ColorPicker(0, x + FlarialGUI::SettingsTextWidth("Color "),
+                                y - Constraints::SpacingConstraint(0.017, textWidth),
+                                settings.getSettingByName<std::string>("color")->value,
+                                settings.getSettingByName<float>("colorOpacity")->value,
+                                settings.getSettingByName<bool>("color_rgb")->value);
 
-        FlarialGUI::ColorPickerWindow(0, settings.getSettingByName<std::string>("color")->value, settings.getSettingByName<float>("colorOpacity")->value, settings.getSettingByName<bool>("color_rgb")->value);
+        FlarialGUI::ColorPickerWindow(0, settings.getSettingByName<std::string>("color")->value,
+                                      settings.getSettingByName<float>("colorOpacity")->value,
+                                      settings.getSettingByName<bool>("color_rgb")->value);
 
     }
 };
--- src/Client/Module/Modules/UpsideDown/UpsideDown.hpp
+++ src/Client/Module/Modules/UpsideDown/UpsideDown.hpp
@@ -3,55 +3,28 @@
 #include "../Module.hpp"
 #include "../../../Events/EventHandler.hpp"
 #include "../../../Client.hpp"
+#include "UpsideDownListener.hpp"
 
 class UpsideDown : public Module {
 
 public:
 
 
-	UpsideDown() : Module("Upside Down", "No need to flip your monitor!!", "\\Flarial\\assets\\upside-down.png", 'o') {
+    UpsideDown() : Module("Upside Down", "No need to flip your monitor!!", R"(\Flarial\assets\upside-down.png)", "") {
 
-		onEnable();
+        Module::setup();
 
-	};
+    };
 
-	void onEnable() override {
+    void onEnable() override {
+        EventHandler::registerOrderedPriorityListener(new UpsideDownListener("UpsideDown", this), 3);
+        Module::onEnable();
+    }
 
-		Module::onEnable();
+    void onDisable() override {
+        EventHandler::unregisterListener("UpsideDown");
+        Module::onDisable();
+    }
 
-		if (settings.getSettingByName<float>("fovvalue") == nullptr) settings.addSetting("fovvalue", 60.00f);
-
-	}
-
-	void onDisable() override {
-
-		Module::onDisable();
-
-	}
-
-	void SettingsRender() override {
-
-		float toggleX = Constraints::PercentageConstraint(0.019, "left");
-		float toggleY = Constraints::PercentageConstraint(0.10, "top");
-
-		const float textWidth = Constraints::RelativeConstraint(0.12, "height", true);
-		const float textHeight = Constraints::RelativeConstraint(0.029, "height", true);
-
-		FlarialGUI::ScrollBar(toggleX, toggleY, 140, Constraints::SpacingConstraint(5.5, textWidth), 2);
-		FlarialGUI::SetScrollView(toggleX, Constraints::PercentageConstraint(0.00, "top"), Constraints::RelativeConstraint(1.0, "width"), Constraints::RelativeConstraint(1.0f, "height"));
-
-        FlarialGUI::FlarialTextWithFont(toggleX, toggleY, L"FOV Value", textWidth * 3.0f, textHeight,
-                                        DWRITE_TEXT_ALIGNMENT_LEADING,
-                                        Constraints::RelativeConstraint(0.12, "height", true),
-                                        DWRITE_FONT_WEIGHT_NORMAL);
-
-		float percent = FlarialGUI::Slider(0, toggleX + FlarialGUI::SettingsTextWidth("FOV Value "),
-                                           toggleY, this->settings.getSettingByName<float>("fovvalue")->value, 110.0f,
-                                           0, 0);
-
-		this->settings.getSettingByName<float>("fovvalue")->value = percent;
-
-		FlarialGUI::UnsetScrollView();
-	}
 };
 
