da7d55e100fffd55dae54f3ede2171e03cd03970
treegfx <nqrbert.yt@gmail.com>
2024-06-21 21:51:45 +0200

    changed splash color to flarial red.

--- src/Client/Hook/Hooks/Visual/FontDrawTransformedHook.hpp
+++ src/Client/Hook/Hooks/Visual/FontDrawTransformedHook.hpp
@@ -7,16 +7,18 @@ class FontDrawTransformedHook : public Hook {
 private:
 
     static void
-    FontDrawTransformedCallback(void *_this, void *screenContext, std::string &str, float x, float y, const MCCColor& color,
+    FontDrawTransformedCallback(void *_this, void *screenContext, std::string &str, float x, float y, MCCColor* color,
                                 int angle, float s, bool centered, float maxWidth) {
-
+        color->r = 1;
+        color->g = 0.137255f;
+        color->b = 0.227451f;
         std::string txt("Farial on top!");
         funcOriginal(_this, screenContext, txt, x, y, color, angle, s, centered, maxWidth);
 
     }
 
 public:
-    typedef void(__thiscall *FontDrawTransformedOriginal)(void *, void *, std::string &, float, float, MCCColor, int,
+    typedef void(__thiscall *FontDrawTransformedOriginal)(void *, void *, std::string &, float, float, MCCColor*, int,
                                                           float, bool, float);
 
     static inline FontDrawTransformedOriginal funcOriginal = nullptr;
