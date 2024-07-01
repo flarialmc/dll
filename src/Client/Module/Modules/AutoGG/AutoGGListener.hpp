2db37f941c496a85dccd11b35ba495dd19ecc88c
St0neHunter <steinjager64@gmail.com>
2024-07-01 16:38:55 +0200

    Auto GG fixed on select few hive game modes

--- src/Client/Module/Modules/AutoGG/AutoGGListener.hpp
+++ src/Client/Module/Modules/AutoGG/AutoGGListener.hpp
@@ -22,7 +22,34 @@ class AutoGGListener : public Listener {
         // TODO: add support for other servers (look for "won the game" text)
         if (id == MinecraftPacketIds::PlaySoundA) {
             auto *pkt = reinterpret_cast<PlaySoundPacket *>(event.getPacket());
-            if (pkt->mName == "ui.toast.challenge_complete_java" || pkt->mName == "raid.horn") {
+            if (pkt->mName == "ui.toast.challenge_complete_java") {
+                triggered = true;
+            }
+
+
+            if (triggered) {
+                std::string win_message = module->settings.getSettingByName<std::string>("text")->value;
+                if (!win_message.empty()) {
+                    auto player = SDK::clientInstance->getLocalPlayer();
+                    std::shared_ptr<Packet> packet = SDK::createPacket(9);
+                    auto *text = reinterpret_cast<TextPacket *>(packet.get());
+
+
+                    text->type = TextPacketType::CHAT;
+                    text->message = win_message;
+                    text->platformId = "";
+                    text->translationNeeded = false;
+                    text->xuid = "";
+                    text->name = player->playerName;
+
+                    SDK::clientInstance->getPacketSender()->sendToServer(text);
+                }
+            }
+        }
+    
+        else if (id == MinecraftPacketIds::Text) {
+            auto *pkt = reinterpret_cast<TextPacket *>(event.getPacket());
+            if (pkt->message == "§c§l» §r§c§lGame OVER!") {
                 triggered = true;
             }
 
@@ -47,7 +74,6 @@ class AutoGGListener : public Listener {
             }
         }
     }
-
     void onPacketSend(PacketEvent &event) override {
         /*if (event.getPacket()->getId() == MinecraftPacketIds::Text) {
             TextPacket *pkt = reinterpret_cast<TextPacket *>(event.getPacket());
