db2bcc161cc308f96c176a9cc0ce64dd2e60d49a
FreezeEngine <mr.springtrap@yandex.ru>
2024-07-05 14:59:09 +0300

    Auto RQ fix + solo mode

--- src/Client/Module/Modules/AutoRQ/AutoRQ.hpp
+++ src/Client/Module/Modules/AutoRQ/AutoRQ.hpp
@@ -30,10 +30,26 @@ public:
     void defaultConfig() override {
         if (settings.getSettingByName<std::string>("command") == nullptr)
             settings.addSetting("command", (std::string)"");
+        if (settings.getSettingByName<bool>("solo") == nullptr)
+            settings.addSetting("solo", false);
     }
 
     void settingsRender() override {
+        float toggleX = Constraints::PercentageConstraint(0.019, "left");
+        float toggleY = Constraints::PercentageConstraint(0.10, "top");
 
+        const float textWidth = Constraints::RelativeConstraint(0.12, "height", true);
+        const float textHeight = Constraints::RelativeConstraint(0.029, "height", true);
+
+        FlarialGUI::FlarialTextWithFont(toggleX + Constraints::SpacingConstraint(0.60, textWidth), toggleY,
+                                        L"Solo mode", textWidth * 6.9f, textHeight,
+                                        DWRITE_TEXT_ALIGNMENT_LEADING, Constraints::SpacingConstraint(1.05, textWidth),
+                                        DWRITE_FONT_WEIGHT_NORMAL);
+
+        if (FlarialGUI::Toggle(5, toggleX, toggleY, this->settings.getSettingByName<bool>(
+                "solo")->value))
+            this->settings.getSettingByName<bool>("solo")->value = !this->settings.getSettingByName<bool>(
+                    "solo")->value;
     }
 };
 
--- src/Client/Module/Modules/AutoRQ/AutoRQListener.hpp
+++ src/Client/Module/Modules/AutoRQ/AutoRQListener.hpp
@@ -33,10 +33,12 @@ class AutoRQListener : public Listener {
     void onPacketReceive(PacketEvent &event) override {
         MinecraftPacketIds id = event.getPacket()->getId();
 
-        if (id == MinecraftPacketIds::Text) {
-            auto *pkt = reinterpret_cast<TextPacket *>(event.getPacket());
+        if (id == MinecraftPacketIds::SetTitle) {
+            if(!module->settings.getSettingByName<bool>("solo")->value) return;
+            auto *pkt = reinterpret_cast<SetTitlePacket *>(event.getPacket());
 
-            if (pkt->message == "§c§l» §r§c§lGame OVER!") {
+            if (pkt->text == "§6§l»§r§c Game Over §6§l«" ||
+                pkt->text == "§6§l»§r§c Game Over §6§l«") {
                 triggered = true;
                 std::shared_ptr<Packet> packet = SDK::createPacket(77);
                 auto* command_packet = reinterpret_cast<CommandRequestPacket*>(packet.get());
@@ -48,43 +50,60 @@ class AutoRQListener : public Listener {
                 command_packet->InternalSource = true;
 
                 SDK::clientInstance->getPacketSender()->sendToServer(command_packet);
-                
+
             } //std::cout << pkt->mName << std::endl;
 
         }
+
         if (id == MinecraftPacketIds::Text) {
-            auto* pkt = reinterpret_cast<TextPacket*>(event.getPacket());
-            //if (pkt->type == TextPacketType::SYSTEM) {
-                std::string textToCheck = "You are connected to server name ";
-                std::string textToCheckToSilence = "You are connected";
-                if (pkt->message.find(textToCheck) != std::string::npos && triggered) {
-                    std::string server = pkt->message.replace(0, textToCheck.length(), "");
-                    std::regex pattern("\\d+");
-                    std::string name = std::regex_replace(server, pattern, "");
-                    FlarialGUI::Notify("Preparing Q: " + name);
-                    std::thread t([name]() {
-                        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-                        
-                        FlarialGUI::Notify("Executing command /q " + name);
-                        
-                        std::shared_ptr<Packet> packet = SDK::createPacket(77);
-                        auto* command_packet = reinterpret_cast<CommandRequestPacket*>(packet.get());
-                        command_packet->command = "/q " + name;
+            auto *pkt = reinterpret_cast<TextPacket *>(event.getPacket());
+            if(!module->settings.getSettingByName<bool>("solo")->value) {
+                if (pkt->message == "§c§l» §r§c§lGame OVER!") {
+                    triggered = true;
+                    std::shared_ptr<Packet> packet = SDK::createPacket(77);
+                    auto *command_packet = reinterpret_cast<CommandRequestPacket *>(packet.get());
 
-                        command_packet->origin.type = CommandOriginType::Player;
+                    command_packet->command = "/connection";
 
-                        command_packet->InternalSource = true;
-                        SDK::clientInstance->getPacketSender()->sendToServer(command_packet);
-                        });
-                    t.detach();
-                    triggered = false;
-                    pkt->message = "";
-                    
-                }
-                else if (pkt->message.find(textToCheckToSilence) != std::string::npos) {
-                    pkt->message = "";
-                }
-            //}
+                    command_packet->origin.type = CommandOriginType::Player;
+
+                    command_packet->InternalSource = true;
+
+                    SDK::clientInstance->getPacketSender()->sendToServer(command_packet);
+                    return;
+                } //std::cout << pkt->mName << std::endl;
+            }
+
+            std::string textToCheck = "You are connected to server name ";
+            std::string textToCheckToSilence = "You are connected";
+
+            if (pkt->message.find(textToCheck) != std::string::npos && triggered) {
+                std::string server = pkt->message.replace(0, textToCheck.length(), "");
+                std::regex pattern("\\d+");
+                std::string name = std::regex_replace(server, pattern, "");
+                FlarialGUI::Notify("Preparing Q: " + name);
+                std::thread t([name]() {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+                    FlarialGUI::Notify("Executing command /q " + name);
+
+                    std::shared_ptr<Packet> packet = SDK::createPacket(77);
+                    auto* command_packet = reinterpret_cast<CommandRequestPacket*>(packet.get());
+                    command_packet->command = "/q " + name;
+
+                    command_packet->origin.type = CommandOriginType::Player;
+
+                    command_packet->InternalSource = true;
+                    SDK::clientInstance->getPacketSender()->sendToServer(command_packet);
+                });
+                t.detach();
+                triggered = false;
+                pkt->message = "";
+
+            }
+            else if (pkt->message.find(textToCheckToSilence) != std::string::npos) {
+                event.cancel();
+            }
         }
     }
 
