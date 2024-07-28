4a6ac29b933fab57167610af5d316b1084b4f4c7
oAnshull <oanshul6969@gmail.com>
2024-07-28 22:30:49 +0530

    better font quality?

--- src/Client/GUI/Engine/Engine.cpp
+++ src/Client/GUI/Engine/Engine.cpp
@@ -547,7 +547,7 @@ void FlarialGUI::FlarialTextWithFont(float x, float y, const wchar_t *text, cons
     if (!FontMap[font]) font = "162";
 
     ImGui::PushFont(FontMap[font]);
-	float fSize = fontSize/450;
+    float fSize = (fontSize / 600) * 1.25;
 
 	ImGui::SetWindowFontScale(fSize);
 
@@ -713,7 +713,10 @@ bool FlarialGUI::LoadFontFromFontFamily(std::string name) {
     if (!fontFilePath.empty()) {
         std::ifstream fontFile(fontFilePath, std::ios::binary);
         if (fontFile) {
-			FontMap[name] = ImGui::GetIO().Fonts->AddFontFromFileTTF(WideToNarrow(fontFilePath).c_str(), 75);
+            ImFontConfig config;
+            config.PixelSnapH = true;
+            config.RasterizerMultiply = 3;
+			FontMap[name] = ImGui::GetIO().Fonts->AddFontFromFileTTF(WideToNarrow(fontFilePath).c_str(), 75, &config);
             if(!FontMap[name]) return false;
             return true;
         }
