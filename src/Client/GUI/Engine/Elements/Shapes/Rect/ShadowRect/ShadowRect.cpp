bbd590c6e03546879c4bac719f1b0e80e6457533
oAnshull <oanshul6969@gmail.com>
2024-07-30 15:55:35 +0530

    fix

--- src/Client/GUI/Engine/Elements/Shapes/Rect/ShadowRect/ShadowRect.cpp
+++ src/Client/GUI/Engine/Elements/Shapes/Rect/ShadowRect/ShadowRect.cpp
@@ -16,7 +16,7 @@ void FlarialGUI::ShadowRect(Vec2<float> pos, Vec2<float> size, D2D_COLOR_F color
 
         Vec2 offset = Vec2(progress * shadowSize, progress * shadowSize);
 
-        ImGui::GetBackgroundDrawList()->AddRect(ImVec2(pos.x - offset.x, pos.y - offset.y), ImVec2(pos.x + size.x + offset.x * 2, pos.y + size.y + offset.y * 2), fadedShadowColor, rounding + progress * shadowSize, 0, 2);
+        ImGui::GetBackgroundDrawList()->AddRect(ImVec2(pos.x - (offset.x), pos.y - (offset.y)), ImVec2(pos.x + size.x + (offset.x * 2), pos.y + size.y + (offset.y * 2)), fadedShadowColor, rounding + progress * shadowSize, 0, 2);
 
         //FlarialGUI::RoundedHollowRect(pos.x - offset.x, pos.y - offset.y, 2.0f, fadedShadowColor, size.x + offset.x*2, size.y + offset.y*2, rounding + progress * shadowSize, rounding + progress * shadowSize);
         
