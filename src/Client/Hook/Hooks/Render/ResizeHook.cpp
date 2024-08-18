2fcc8a2c3364bcab4ae1fd7985a48a8050a50b1a
FreezeEngine <mr.springtrap@yandex.ru>
2024-08-18 23:08:59 +0300

    DX11 no render fix

--- src/Client/Hook/Hooks/Render/ResizeHook.cpp
+++ src/Client/Hook/Hooks/Render/ResizeHook.cpp
@@ -64,37 +64,41 @@ void ResizeHook::cleanShit(bool isResize) {
     FlarialGUI::textFormatCache.clear();
     FlarialGUI::textLayoutCache.clear();
 
-    if (SwapchainHook::init && SwapchainHook::d3d11On12Device != nullptr) {
+    if (SwapchainHook::init) {
 
         SwapchainHook::D3D12DescriptorHeap.put_void();
 
-        if (!isResize && SwapchainHook::queue != nullptr) {
-            auto resources = SwapchainHook::D3D11Resources.data()->get();
-            SwapchainHook::d3d11On12Device->ReleaseWrappedResources(&resources,
-                                                                    static_cast<UINT>(SwapchainHook::D3D11Resources.size()));
+        if (!isResize && SwapchainHook::queue != nullptr && SwapchainHook::d3d11On12Device != nullptr) {
+            for (auto& resource : SwapchainHook::D3D11Resources) {
+                ID3D11Resource* rawResource = resource.get();
+                SwapchainHook::d3d11On12Device->ReleaseWrappedResources(&rawResource, 1);
+            }
         }
+
+        if(SwapchainHook::context != nullptr)
+            SwapchainHook::context->Flush();
+
+        SwapchainHook::context.put_void();
+
+        SwapchainHook::d3d11On12Device.put_void();
 
         SwapchainHook::D2D1Bitmaps.clear();
         SwapchainHook::D3D11Resources.clear();
         SwapchainHook::DXGISurfaces.clear();
 
-        SwapchainHook::context->Flush();
-        // TODO: release all render effects here
-        SwapchainHook::context.put_void();
-
-        D2D::surface.put_void();
-        FlarialGUI::blur.put_void();
-        FlarialGUI::shadow_blur.put_void();
-        FlarialGUI::blurbrush.put_void();
-        FlarialGUI::factory.put_void();
-        FlarialGUI::writeFactory.put_void();
-        FlarialGUI::screen_bitmap_cache.put_void();
-        FlarialGUI::blur_bitmap_cache.put_void();
-
-        SwapchainHook::d3d11On12Device.put_void();
-
         if (!isResize) Memory::SafeRelease(SwapchainHook::queue);
     }
+
+    // release all render effects here
+
+    D2D::surface.put_void();
+    FlarialGUI::blur.put_void();
+    FlarialGUI::shadow_blur.put_void();
+    FlarialGUI::blurbrush.put_void();
+    FlarialGUI::factory.put_void();
+    FlarialGUI::writeFactory.put_void();
+    FlarialGUI::screen_bitmap_cache.put_void();
+    FlarialGUI::blur_bitmap_cache.put_void();
 
     SwapchainHook::D2D1Bitmap.put_void();
 
