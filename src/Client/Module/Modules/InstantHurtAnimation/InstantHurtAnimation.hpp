c21c4d340c5148338159ee4cd6f18595cdc52508
FreezeEngine <mr.springtrap@yandex.ru>
2024-09-05 10:34:45 +0300

    Instant hurt animation improvements

--- src/Client/Module/Modules/InstantHurtAnimation/InstantHurtAnimation.hpp
+++ src/Client/Module/Modules/InstantHurtAnimation/InstantHurtAnimation.hpp
@@ -25,9 +25,8 @@ public:
     }
 
     void defaultConfig() override {
-
-        if (settings.getSettingByName<bool>("tryToExcludeTeam") == nullptr) settings.addSetting("tryToExcludeTeam", false);
-
+        if (settings.getSettingByName<bool>("onlyWithArmor") == nullptr) settings.addSetting("onlyWithArmor", true);
+        if (settings.getSettingByName<bool>("tryToExcludeTeam") == nullptr) settings.addSetting("tryToExcludeTeam", true);
     }
 
     void settingsRender() override {
@@ -47,5 +46,15 @@ public:
                 "tryToExcludeTeam")->value))
             this->settings.getSettingByName<bool>("tryToExcludeTeam")->value = !this->settings.getSettingByName<bool>(
                     "tryToExcludeTeam")->value;
+
+        FlarialGUI::FlarialTextWithFont(toggleX + Constraints::SpacingConstraint(0.60, textWidth), toggleY,
+                                        L"Only with armor", textWidth * 6.9f, textHeight,
+                                        DWRITE_TEXT_ALIGNMENT_LEADING, Constraints::SpacingConstraint(1.05, textWidth),
+                                        DWRITE_FONT_WEIGHT_NORMAL);
+
+        if (FlarialGUI::Toggle(1, toggleX, toggleY, this->settings.getSettingByName<bool>(
+                "onlyWithArmor")->value))
+            this->settings.getSettingByName<bool>("onlyWithArmor")->value = !this->settings.getSettingByName<bool>(
+                    "onlyWithArmor")->value;
     }
 };
