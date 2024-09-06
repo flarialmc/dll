c41b8f16bbbbdb12c1af23156eb74025a585cae5
FreezeEngine <mr.springtrap@yandex.ru>
2024-09-06 17:25:51 +0300

    Bump up commit hash

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -19,7 +19,7 @@ std::string Client::settingspath = Utils::getRoamingPath() + R"(\Flarial\Config\
 Settings Client::settings = Settings();
 bool notifiedOfConnectionIssue = false;
 // UPDATE THIS TO THE LATEST GITHUB COMMIT AFTER CHANGING ANYHTHING
-std::string current_commit = "cfbd2c2f";
+std::string current_commit = "f3ed456";
 
 void DownloadAndSave(const std::string& url, const std::string& path) {
 
