dcc310a61724b20941a4e0f7d2eddd143b56ed50
Bari <58800830+TheBarii@users.noreply.github.com>
2024-10-19 23:27:46 +0600

    every 10 mins instead

--- main.cpp
+++ main.cpp
@@ -109,7 +109,7 @@ DWORD WINAPI init(HMODULE real)
                             lastOnlineUsersFetchTime = now;
                         }
 
-                        if(SDK::clientInstance && onlineAnnouncementElapsed >= std::chrono::minutes(3)) {
+                        if(SDK::clientInstance && onlineAnnouncementElapsed >= std::chrono::minutes(10)) {
                             SDK::clientInstance->getGuiData()->displayClientMessage("§khiii §r §n§l§4FLARIAL §r§khiii \n§r§cDonate to Flarial! §ehttps://flarial.xyz/donate\n§9Join our discord! §ehttps://flarial.xyz/discord");
                             lastAnnouncementTime = now;
                         }
