7cfcf5e176b466370c7d483f7e6e5c62c9881974
treegfx <nqrbert.yt@gmail.com>
2024-11-07 13:28:22 +0100

    clean up

--- src/Client/Module/Modules/Waypoints/Waypoints.hpp
+++ src/Client/Module/Modules/Waypoints/Waypoints.hpp
@@ -40,34 +40,12 @@ public:
             !SDK::clientInstance->getLocalPlayer()->getLevel())
             return;
 
-        auto player = SDK::clientInstance->getLocalPlayer();
-
         if (FlarialGUI::inMenu || SDK::getCurrentScreen() != "hud_screen") return;
 
-        if (player != nullptr) {
-             
-            D2D1_COLOR_F color2 = FlarialGUI::rgbColor;
-            
-            std::lock_guard<std::mutex> guard(renderMtx);
-                // Retrieve the thickness setting value from the module settings
+        Vec2<float> screen;
 
-            float lineWidth = 1.01f;
-
-            // Get the render position of the player and compute the distance to the AABB lower bound
-            float distance = player->getRenderPositionComponent()->renderPos.dist(Vec3{ 0.0F, 0.0F, 0.0F });
-
-            // Compute the scaling factor based on the distance, ensuring it does not exceed the max distance of 30
-            float scaleFactor = 1.f - (distance / 30.0f);
-
-            // Calculate the line width, ensuring it does not fall below a minimum threshold of 0.5
-            lineWidth = 1.01f * scaleFactor;
-
-            DrawUtils::addBox(Vec3{ 0.0F, 0.0F, 0.0F }, Vec3{ 1.0F, 1.0F, 1.0F }, lineWidth, 1, color2);
-            Vec2<float> screen;
-
-            if (Matrix::WorldToScreen(Vec3{ 0.0F, 0.0F, 0.0F }, screen)) {
-                FlarialGUI::FlarialTextWithFont(screen.x, screen.y, L"waypoint 1", 500, 0, DWRITE_TEXT_ALIGNMENT_LEADING, Constraints::RelativeConstraint(0.215f, "height", true), DWRITE_FONT_WEIGHT_NORMAL, FlarialGUI::rgbColor, true);
-            }
+        if (Matrix::WorldToScreen(Vec3{ 0.0F, 0.0F, 0.0F }, screen)) {
+            FlarialGUI::FlarialTextWithFont(screen.x, screen.y, L"waypoint 1", 500, 0, DWRITE_TEXT_ALIGNMENT_LEADING, Constraints::RelativeConstraint(0.215f, "height", true), DWRITE_FONT_WEIGHT_NORMAL, FlarialGUI::rgbColor, true);
         }
     }
 };
