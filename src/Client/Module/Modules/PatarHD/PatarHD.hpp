e01bf2a75ef064b57ddb8c2feb05b758002a0403
treegfx <nqrbert.yt@gmail.com>
2024-11-10 14:56:39 +0100

    jqms mod

--- src/Client/Module/Modules/PatarHD/PatarHD.hpp
+++ src/Client/Module/Modules/PatarHD/PatarHD.hpp
@@ -9,6 +9,9 @@ private:
     float y = 0;
     float xv = 1;
     float yv = 1;
+    int index = 0;
+    int lastTime = 0;
+    std::chrono::steady_clock::time_point time;
 public:
 
     PatarHD() : Module("PatarHD", "Who is this now?", IDR_SKULL_PNG, "") {
@@ -18,6 +21,7 @@ public:
     void onEnable() override {
         Listen(this, RenderEvent, &PatarHD::onRender)
         Module::onEnable();
+        reset();
     }
 
     void onDisable() override {
@@ -30,6 +34,7 @@ public:
         if (settings.getSettingByName<float>("xveloc") == nullptr) settings.addSetting("xveloc", 1.0f);
         if (settings.getSettingByName<float>("yveloc") == nullptr) settings.addSetting("yveloc", 0.69f);
         if (settings.getSettingByName<float>("scale") == nullptr) settings.addSetting("scale", 1.0f);
+        if (this->settings.getSettingByName<bool>("jqms") == nullptr) settings.addSetting("jqms", false);
     }
 
     void settingsRender(float settingsOffset) override {
@@ -48,6 +53,7 @@ public:
         this->addHeader("General");
 
         this->addToggle("DVD Mode", "See for yourself", this->settings.getSettingByName<bool>("dvdmode")->value);
+        this->addToggle("Jqms Mode", "idk man i thought it was funny", this->settings.getSettingByName<bool>("jqms")->value);
         this->addSlider("Scale", "", this->settings.getSettingByName<float>("scale")->value, 5.0F);
         if (this->settings.getSettingByName<bool>("dvdmode")->value) {
             this->addHeader("DVD Mode");
@@ -61,11 +67,25 @@ public:
 
     void onRender(RenderEvent &event) {
         if (SDK::currentScreen != "hud_screen") return;
-
         float s = Constraints::RelativeConstraint(0.35, "height", true) * this->settings.getSettingByName<float>("scale")->value;
-
+        int draw = 165;
+        if (this->settings.getSettingByName<bool>("jqms")->value)
+        {
+            if (getMs() >= 70) {
+                reset();
+                if (index == 32) {
+                    index = 0;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            
+            draw = 184 + index;
+        }
         if (this->settings.getSettingByName<bool>("dvdmode")->value) {
-            FlarialGUI::image(IDR_PATAR_JPG, D2D1::RectF(x, y, x + s, y + s), "JPG");
+            FlarialGUI::image(draw, D2D1::RectF(x, y, x + s, y + s), "JPG");
 
             x += this->settings.getSettingByName<float>("xveloc")->value * xv;
             y += this->settings.getSettingByName<float>("yveloc")->value * yv;
@@ -77,7 +97,16 @@ public:
         } else {
             float s2 = s/2 ;
             Vec2<float> center = Vec2<float>{ MC::windowSize.x / 2, MC::windowSize.y / 2 };
-            FlarialGUI::image(IDR_PATAR_JPG, D2D1::RectF(center.x - s2, center.y - s2, center.x + s2, center.y + s2), "JPG");
+            FlarialGUI::image(draw, D2D1::RectF(center.x - s2, center.y - s2, center.x + s2, center.y + s2), "JPG");
         }
+    }
+
+    int getMs() {
+        auto now = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - time);
+        return static_cast<int>(duration.count());
+    }
+    void reset() {
+        time = std::chrono::steady_clock::now();
     }
 };
