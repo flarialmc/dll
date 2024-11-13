234255ddb3c97d9311888cc495ec8b12cb345244
St0neHunter <steinjager64@gmail.com>
2024-11-13 21:17:15 +0100

    Thx withors for the help

--- src/Scripting/Functions/GUI.hpp
+++ src/Scripting/Functions/GUI.hpp
@@ -3,6 +3,7 @@
 #include <lua.hpp>
 #include <d2d1helper.h>
 #include "../../Client/GUI/Engine/Engine.hpp"
+#include <string>
 
 
 namespace GUI {
@@ -38,9 +39,42 @@ namespace GUI {
         lua_pushcfunction(L, lua_GUI_RoundedRect);
         lua_setfield(L, -2, "RoundedRect");
 
+        lua_pushcfunction(L, lua_GUI_RoundedHollowRect);
+        lua_setfield(L, -2, "RoundedHollowRect");
+
         lua_pushcfunction(L, lua_GUI_Color);
-        lua_setfield(L, -2, "Color");
+        lua_setfield(L, -2, "Color");        
 
         lua_setglobal(L, "GUI");
+    }
+
+    int lua_GUI_FlarialTextWithFont(lua_State *L) {
+        int x = luaL_checkinteger(L, 1);
+        int y = luaL_checkinteger(L, 2);
+
+        std::string text = luaL_checkstring(L, 3);
+
+        float width = luaL_checknumber(L, 4);
+        float height = luaL_checknumber(L, 5);
+
+    }
+
+    int lua_GUI_RoundedHollowRect(lua_State *L) {
+        float x = luaL_checkinteger(L, 1);
+        float y = luaL_checkinteger(L, 2);
+        auto color = static_cast<D2D1::ColorF *>(lua_touserdata(L, 3));
+
+        _D3DCOLORVALUE d3dColorValue;
+        d3dColorValue.r = color->r;
+        d3dColorValue.g = color->g;
+        d3dColorValue.b = color->b;
+        d3dColorValue.a = color->a;
+
+        float height = luaL_checknumber(L, 4);
+        float width = luaL_checknumber(L, 5);
+        float rounding = luaL_checknumber(L, 6);
+        float shadowSize = luaL_checknumber(L, 7);
+        FlarialGUI::ShadowRect(Vec2(x,y), Vec2(height, width), d3dColorValue, rounding, shadowSize);
+
     }
 }
