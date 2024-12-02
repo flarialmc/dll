823e63f5594568dfac362a85b6e3b1fd2dfc8295
TTF-fog <ttf431@proton.me>
2024-12-02 12:21:42 +0530

    update players to add: getSelectedSlot returnPosArray (returns x,y,z as a array) returnPlayerList (returns names of players in list) PROTOTYPE :D

--- src/Scripting/Functions/Player.hpp
+++ src/Scripting/Functions/Player.hpp
@@ -6,7 +6,7 @@
 #include <string>
 #include <codecvt>
 #include "../../SDK/Client/Actor/Player.hpp"
-
+#include <algorithm>
 
 namespace player {
     int lua_getPlayerX(lua_State* L) {
@@ -14,9 +14,36 @@ namespace player {
             return 0;
         }
         lua_pushnumber(L, SDK::clientInstance->getLocalPlayer()->getPosition()->x);
-        
         return 1;
     }
+    int lua_returnPosArray(lua_State* L) {
+        if (SDK::clientInstance != nullptr) {
+            return 0;
+        }
+        //this should work
+        lua_newtable(L);
+        lua_pushnumber(L, SDK::clientInstance->getLocalPlayer()->getPosition()->x);
+        lua_rawseti(L, -2, 1);
+        lua_pushnumber(L, SDK::clientInstance->getLocalPlayer()->getPosition()->y);
+        lua_rawseti(L, -2, 2);
+        lua_pushnumber(L, SDK::clientInstance->getLocalPlayer()->getPosition()->z);
+        lua_rawseti(L, -2, 3);
+        return 1;
+    }
+    int lua_returnPlayerList(lua_State* L) {
+        if (SDK::clientInstance != nullptr) {
+            return 0;
+        }
+        lua_newtable(L); //create new table for players
+        const std::vector<Actor *> playerList= SDK::clientInstance->getLocalPlayer()->getLevel()->getRuntimeActorList() ;
+        for (const auto &player : playerList){
+            lua_pushstring(L,player->getNametag()->c_str()); //push strng of player name to table
+            lua_rawseti(L, -2, std::distance((playerList.begin()), std::find(playerList.begin(), playerList.end(),player))+1);
+            //set item at index of player + 1 (lua arrays are not 0-indexed;
+        };
+        return 1;
+    }
+
 
     int lua_getPlayerY(lua_State* L) {
         if (SDK::clientInstance != nullptr){
@@ -26,6 +53,14 @@ namespace player {
         
         return 1;
     }
+    int lua_getPlayerSlot(lua_State* L) {
+        if (SDK::clientInstance != nullptr ) {
+            return 0;
+        }
+        lua_pushinteger(L,SDK::clientInstance->getLocalPlayer()->getSupplies()->getSelectedSlot());
+        return 1;
+    }
+
 
     int lua_getPlayerZ(lua_State* L) {
         if (SDK::clientInstance != nullptr){
@@ -61,7 +96,12 @@ namespace player {
 
         lua_pushcfunction(L, lua_getPlayerZ);
         lua_setfield(L, -2, "getZ");
-
+        lua_pushcfunction(L, lua_getPlayerSlot);
+        lua_setfield(L, -2, "getSlot");
+        lua_pushcfunction(L, lua_returnPosArray);
+        lua_setfield(L, -2, "returnPosArray");
+        lua_pushcfunction(L, lua_returnPlayerList);
+        lua_setfield(L, -2, "returnPlayerList");
         lua_setglobal(L, "Player");
 
 
