5b79a45254f20c992aefbbe2612d65987cdf0fee
TTF-fog <adh.ishaan10@gmail.com>
2025-03-04 17:57:42 +0530

    script utils (just lua array now) playerlist coming next :tm:

---
+++ src/Utils/ScriptUtils.hpp
@@ -0,0 +1,12 @@
+#include <map>
+#include <string>
+#include <vector>
+//
+// Created by ttf on 3/4/25.
+//
+//development utilities for scripting
+namespace ScriptUtils {
+   int lua_pushArray(std::vector<double> &test);
+   int lua_pushArray(std::vector<std::string> &test);
+
+   }
