731343d62f37f93736706aaf8fcfb6e00875dfc3
Bari <58800830+TheBarii@users.noreply.github.com>
2025-03-11 13:47:08 +0600

    i tried ig

--- src/Client/Module/Modules/ClickGUI/ClickGUI.hpp
+++ src/Client/Module/Modules/ClickGUI/ClickGUI.hpp
@@ -88,7 +88,7 @@ public:
     void onPacketReceive(PacketEvent &event) {
         if (event.getPacket()->getId() == MinecraftPacketIds::Text) {
             auto *pkt = reinterpret_cast<TextPacket *>(event.getPacket());
-            if (ClickGUI::containsAny(String::removeNonAlphanumeric(String::removeColorCodes(pkt->message)))){ pkt->message = "§f[§4FLARIAL§f]§r " + pkt->message;}
+            if (ClickGUI::containsAny(String::removeNonAlphanumeric(String::removeColorCodes(pkt->message)))){ pkt->message = fmt::format("§f[{}FLARIAL§f]§r ", "§4") + pkt->message;}
         }
     }
 
