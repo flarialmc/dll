d82e437e8e52d6c658161db67fa9507786b1f776
oAnshull <oanshul6969@gmail.com>
2025-03-17 14:30:47 +0530

    Big update to viewmodel (trust)

--- src/Client/Module/Modules/ViewModel/ViewModel.hpp
+++ src/Client/Module/Modules/ViewModel/ViewModel.hpp
@@ -11,39 +11,64 @@ public:
 
     void onEnable() override {
         Listen(this, FOVEvent, &ViewModel::onGetFOV)
+        Listen(this, RenderItemInHandEvent, &ViewModel::onRenderItemInHand)
         Module::onEnable();
 
     }
 
     void onDisable() override {
         Deafen(this, FOVEvent, &ViewModel::onGetFOV)
+        Deafen(this, RenderItemInHandEvent, &ViewModel::onRenderItemInHand)
+        if (Matrixed) {
+            auto& matrix = SDK::clientInstance->getCamera().getWorldMatrixStack().top().matrix;
+            matrix = OriginalMatrix;
+        }
+
         Module::onDisable();
     }
 
     void defaultConfig() override { Module::defaultConfig();
         if (settings.getSettingByName<float>("itemfov") == nullptr) settings.addSetting("itemfov", 70.0f);
+
+        if (settings.getSettingByName<float>("posx") == nullptr) settings.addSetting("posx", 6.0f);
+        if (settings.getSettingByName<float>("posy") == nullptr) settings.addSetting("posy", 6.0f);
+        if (settings.getSettingByName<float>("posz") == nullptr) settings.addSetting("posz", 6.0f);
+
+        if (settings.getSettingByName<float>("rotangle") == nullptr) settings.addSetting("rotangle", 0.0f);
+
+        if (settings.getSettingByName<float>("rotx") == nullptr) settings.addSetting("rotx", 0.0f);
+        if (settings.getSettingByName<float>("roty") == nullptr) settings.addSetting("roty", 0.0f);
+        if (settings.getSettingByName<float>("rotz") == nullptr) settings.addSetting("rotz", 0.0f);
     }
 
     void settingsRender(float settingsOffset) override {
 
-        float textWidth = Constraints::RelativeConstraint(0.12, "height", true);
-        const float textHeight = Constraints::RelativeConstraint(0.029, "height", true);
+        float x = Constraints::PercentageConstraint(0.019, "left");
+        float y = Constraints::PercentageConstraint(0.10, "top");
 
-        float toggleX = Constraints::PercentageConstraint(0.019, "left");
-        float toggleY = Constraints::PercentageConstraint(0.10, "top");
-
-        FlarialGUI::FlarialTextWithFont(toggleX, toggleY, L"Item FOV", textWidth * 3.0f, textHeight,
-                                        DWRITE_TEXT_ALIGNMENT_LEADING,
-                                        Constraints::RelativeConstraint(0.12, "height", true),
-                                        DWRITE_FONT_WEIGHT_NORMAL);
+        const float scrollviewWidth = Constraints::RelativeConstraint(0.5, "height", true);
 
 
-        float value = FlarialGUI::Slider(0, toggleX + FlarialGUI::SettingsTextWidth("Item FOV "),
-                                           toggleY, this->settings.getSettingByName<float>("itemfov")->value, 180, 0,
-                                           false);
+        FlarialGUI::ScrollBar(x, y, 140, Constraints::SpacingConstraint(5.5, scrollviewWidth), 2);
+        FlarialGUI::SetScrollView(x - settingsOffset, Constraints::PercentageConstraint(0.00, "top"),
+            Constraints::RelativeConstraint(1.0, "width"),
+            Constraints::RelativeConstraint(0.88f, "height"));
 
-        this->settings.getSettingByName<float>("itemfov")->value = value;
 
+        this->addHeader("Main");
+        this->addSlider("Item FOV", "Changes the FOV appearance of the item.", this->settings.getSettingByName<float>("itemfov")->value, 180);
+
+        this->addSlider("Position X", "Changes the position in the X axis", this->settings.getSettingByName<float>("posx")->value, 12);
+        this->addSlider("Position Y", "Changes the position in the Y axis", this->settings.getSettingByName<float>("posy")->value, 12);
+        this->addSlider("Position Z", "Changes the position in the Z axis", this->settings.getSettingByName<float>("posz")->value, 12);
+
+        this->addSlider("Rotation Angle", "Changes the rotation angle of the item", this->settings.getSettingByName<float>("rotangle")->value, 360);
+        this->addSlider("Rotation X", "Changes the rotation in the X axis", this->settings.getSettingByName<float>("rotx")->value, 360);
+        this->addSlider("Rotation Y", "Changes the rotation in the Y axis", this->settings.getSettingByName<float>("roty")->value, 360);
+        this->addSlider("Rotation Z", "Changes the rotation in the Z axis", this->settings.getSettingByName<float>("rotz")->value, 360);
+
+        FlarialGUI::UnsetScrollView();
+        this->resetPadding();
     }
 
     void onGetFOV(FOVEvent &event) {
@@ -52,4 +77,26 @@ public:
 
         event.setFOV(this->settings.getSettingByName<float>("itemfov")->value);
     }
+
+    void onRenderItemInHand(RenderItemInHandEvent& event) {
+        auto& matrix = SDK::clientInstance->getCamera().getWorldMatrixStack().top().matrix;
+
+        if (!Matrixed) OriginalMatrix = matrix;
+
+        auto posx = this->settings.getSettingByName<float>("posx")->value;
+        auto posy = this->settings.getSettingByName<float>("posy")->value;
+        auto posz = this->settings.getSettingByName<float>("posz")->value;
+
+        auto rotx = this->settings.getSettingByName<float>("rotx")->value;
+        auto roty = this->settings.getSettingByName<float>("roty")->value;
+        auto rotz= this->settings.getSettingByName<float>("rotz")->value;
+
+        auto rotAngle = this->settings.getSettingByName<float>("rotangle")->value;
+
+        matrix = glm::translate<float>(matrix, glm::vec3(posx-4, posy-4, posz-4));
+        matrix = glm::rotate<float>(matrix, glm::radians(rotAngle), glm::vec3(rotx, roty, rotz));
+    }
+
+    glm::mat4x4 OriginalMatrix;
+    bool Matrixed = false;
 };
