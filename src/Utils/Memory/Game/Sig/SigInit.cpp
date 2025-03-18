5bbfb22622ee574919b91ff969f0f366072c33f3
oAnshull <oanshul6969@gmail.com>
2025-03-18 14:30:18 +0530

    bawli gaand update

--- src/Utils/Memory/Game/Sig/SigInit.cpp
+++ src/Utils/Memory/Game/Sig/SigInit.cpp
@@ -558,4 +558,6 @@ void SigInit::init2030() {
     ADD_SIG("ContainerScreenController::_onContainerSlotHovered", "40 53 48 83 EC 40 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 44 24 30 41 8B D8");// "40 ? 55 56 57 41 ? 48 81 EC ? ? ? ? 41 8B ? 48 8B");
 
     ADD_SIG("BobView", "40 53 56 48 83 EC 78 ? ? 7C");
+
+    ADD_SIG("ItemInHandRenderer::renderItem", "E8 ?? ?? ?? ?? 41 0F 11 76 ??");
 }
