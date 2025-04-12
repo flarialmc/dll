a4f906cf33c154ea407747e07f63c2e509b9dac6
chyves <82107846+notchyves@users.noreply.github.com>
2025-04-12 17:10:11 -0400

    Create MinimalViewBobbing.hpp

---
+++ src/Client/Module/Modules/MinimalViewBobbing/MinimalViewBobbing.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "../Module.hpp"
+
+
+class MinimalViewBobbing : public Module {
+private:
+    static inline std::vector<uint8_t> original;
+    static inline uintptr_t address;
+public:
+    MinimalViewBobbing() : Module("MinimalViewBobbing", "Prevent camera shake when view bobbing is on.",
+                           IDR_NAMETAG_PNG, "") {
+
+        address = GET_SIG_ADDRESS("MinimalViewBobbing");
+
+        original.resize(6);
+        Memory::copyBytes((LPVOID) address, original.data(), 6);
+
+        Module::setup();
+    };
+
+    void onEnable() override {
+        patch();
+        Module::onEnable();
+    }
+
+    void onDisable() override {
+        unpatch();
+        Module::onDisable();
+    }
+
+    static void patch() {
+        Memory::nopBytes((void *)address, 6);
+    }
+
+    static void unpatch() {
+        Memory::patchBytes((void *)address, original.data(), original.size());
+    }
+};
