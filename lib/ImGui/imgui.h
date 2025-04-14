47b129d19ac0c53b06bf0e3846752143c8d32a3a
oAnshull <oanshul6969@gmail.com>
2025-04-14 15:28:06 +0530

    Refactor Engine.cpp (biggest mistake of my life)

--- lib/ImGui/imgui.h
+++ lib/ImGui/imgui.h
@@ -296,6 +296,7 @@ struct ImVec2
     float                                   x, y;
     constexpr ImVec2()                      : x(0.0f), y(0.0f) { }
     constexpr ImVec2(float _x, float _y)    : x(_x), y(_y) { }
+    ImVec2 operator-(ImVec2 vec)            { return ImVec2(x - vec.x, y - vec.y); }
     float& operator[] (size_t idx)          { IM_ASSERT(idx == 0 || idx == 1); return ((float*)(void*)(char*)this)[idx]; } // We very rarely use this [] operator, so the assert overhead is fine.
     float  operator[] (size_t idx) const    { IM_ASSERT(idx == 0 || idx == 1); return ((const float*)(const void*)(const char*)this)[idx]; }
 #ifdef IM_VEC2_CLASS_EXTRA
