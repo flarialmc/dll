1d3fb1395b8964270a0acde92cff5de5d1244ef6
oAnshull <oanshul6969@gmail.com>
2025-04-27 16:58:15 +0530

    MAJOR BIG UPDATE TO MEDS

--- src/Client/Module/Modules/202020/202020.hpp
+++ src/Client/Module/Modules/202020/202020.hpp
@@ -6,6 +6,7 @@ class Module202020 : public Module {
 public:
     std::chrono::steady_clock::time_point now;
     std::chrono::steady_clock::time_point last;
+    std::chrono::steady_clock::time_point blackscreen;
     std::chrono::seconds elapsed;
 
     Module202020() : Module("Meds", "Implements the medical 202020 rule\ninto minecraft.", IDR_TIME_PNG, "") {
@@ -15,26 +16,52 @@ public:
     void onEnable() override {
         now = std::chrono::steady_clock::now();
         last = now;
-        Listen(this, TickEvent, &Module202020::onTick)
+        Listen(this, RenderEvent, &Module202020::onRender)
         Module::onEnable();
     }
 
     void onDisable() override {
-        Deafen(this, TickEvent, &Module202020::onTick)
+        Deafen(this, RenderEvent, &Module202020::onRender)
         Module::onDisable();
     }
 
-    void defaultConfig() override { Module::defaultConfig(); }
-
-    void settingsRender(float settingsOffset) override {}
+    void defaultConfig() override { Module::defaultConfig();
     
-    void onTick(TickEvent &event) {
+    if (settings.getSettingByName<bool>("extreme") == nullptr)
+        settings.addSetting("extreme", false);
+    }
+
+    void settingsRender(float settingsOffset) override {
+        float x = Constraints::PercentageConstraint(0.019, "left");
+        float y = Constraints::PercentageConstraint(0.10, "top");
+
+        const float scrollviewWidth = Constraints::RelativeConstraint(0.5, "height", true);
+
+        FlarialGUI::ScrollBar(x, y, 140, Constraints::SpacingConstraint(5.5, scrollviewWidth), 2);
+        FlarialGUI::SetScrollView(x - settingsOffset, Constraints::PercentageConstraint(0.00, "top"),
+            Constraints::RelativeConstraint(1.0, "width"),
+            Constraints::RelativeConstraint(0.88f, "height"));
+
+        this->addHeader("Main");
+        this->addToggle("Extreme Mode", "", this->settings.getSettingByName<bool>("extreme")->value);
+
+        FlarialGUI::UnsetScrollView();
+        this->resetPadding();
+    }
+    
+    void onRender(RenderEvent &event) {
         now = std::chrono::steady_clock::now();
         elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - last);
 
         if (elapsed >= std::chrono::minutes(20)) {
-            FlarialGUI::Notify("Look at something 20 feet away for 20 seconds!");
+            if (!this->settings.getSettingByName<bool>("extreme")->value)
+                FlarialGUI::Notify("Look at something 20 feet away for 20 seconds!");
             last = now;
+            blackscreen = now;
+        }
+
+        if (this->settings.getSettingByName<bool>("extreme")->value and std::chrono::duration_cast<std::chrono::seconds>(now - last) < std::chrono::seconds(20) and elapsed >= std::chrono::minutes(20)) {
+            FlarialGUI::RoundedRect(0, 0, { 0.f, 0.f, 0.f, 1.f }, Constraints::PercentageConstraint(1, "left"), Constraints::PercentageConstraint(1, "top"), 0, 0);
         }
     }
 };
