c0c0939d4176bd1d3d527608bbbb269f5884964a
prathfr <yadav.pratham@outlook.com>
2025-05-25 02:25:04 +0530

    beta: sigma edit 3 am goofy LEAKED GONE WRONG!

--- src/Client/Module/Modules/Coords/Coords.hpp
+++ src/Client/Module/Modules/Coords/Coords.hpp
@@ -6,6 +6,8 @@
 #include "../Module.hpp"
 #include "Events/Render/RenderUnderUIEvent.hpp"
 
+typedef std::unordered_map<std::string, std::string> StringMap;
+
 class Coords : public Module {
 
 public:
@@ -31,12 +33,19 @@ public:
         if (settings.getSettingByName<bool>("responsivewidth") == nullptr) settings.addSetting("responsivewidth", true);
         Module::defaultConfig();
         if (settings.getSettingByName<std::string>("text") == nullptr)
-            settings.addSetting("text", (std::string)"X: {X} Y: {Y} Z: {Z}");
+            settings.addSetting("text", (std::string)"{D}  X: {X} Y: {Y} Z: {Z}");
 
         if (settings.getSettingByName<float>("textscale") == nullptr) settings.addSetting("textscale", 0.80f);
 
         if (settings.getSettingByName<bool>("showDecimals") == nullptr) settings.addSetting("showDecimals", false);
         if (settings.getSettingByName<float>("decimalCount") == nullptr) settings.addSetting("decimalCount", 2.f);
+
+		if (settings.getSettingByName<bool>("showOtherDimCoords") == nullptr) settings.addSetting("showOtherDimCoords", false);
+        if (settings.getSettingByName<bool>("showDimName") == nullptr) settings.addSetting("showDimName", false);
+
+        if (settings.getSettingByName<std::string>("OverworldFormat") == nullptr) settings.addSetting("OverworldFormat", (std::string)"{dim}");
+        if (settings.getSettingByName<std::string>("NetherFormat") == nullptr) settings.addSetting("NetherFormat", (std::string)"{dim}");
+        if (settings.getSettingByName<std::string>("TheEndFormat") == nullptr) settings.addSetting("TheEndFormat", (std::string)"{dim}");
     }
 
     void settingsRender(float settingsOffset) override {
@@ -76,6 +85,15 @@ public:
 
         this->extraPadding();
 
+        this->addHeader("Module Settings");
+		this->addToggle("Show Other Dimension Coords", "Shows the other dimension\'s coordinates along with overworld coordinates.", this->settings.getSettingByName<bool>("showOtherDimCoords")->value);
+        this->addToggle("Show Dimension Name", "Shows the dimension name.", this->settings.getSettingByName<bool>("showDimName")->value);
+        this->addTextBox("Overworld Dimension Format", "", settings.getSettingByName<std::string>("OverworldFormat")->value);
+        this->addTextBox("Nether Dimension Format", "", settings.getSettingByName<std::string>("NetherFormat")->value);
+        this->addTextBox("TheEnd Dimension Format", "", settings.getSettingByName<std::string>("TheEndFormat")->value);
+
+        this->extraPadding();
+
         this->addHeader("Colors");
         this->addColorPicker("Background Color", "", settings.getSettingByName<std::string>("bgColor")->value,
             settings.getSettingByName<float>("bgOpacity")->value,
@@ -110,32 +128,100 @@ public:
     void onRenderUnderUI(RenderUnderUIEvent& event) {
         //Blur::RenderBlur(event.RTV, 3, 5.f);
     }
+
+    StringMap getCoords(float multiplier) {
+        Vec3<float>* pos = SDK::clientInstance->getLocalPlayer()->getPosition();
+
+        int decimalsToShow = this->settings.getSettingByName<bool>("showDecimals")->value ? std::floor(this->settings.getSettingByName<float>("decimalCount")->value) : -1;
+
+        std::string xstr = std::to_string(pos->x * multiplier);
+        std::string ystr = std::to_string(pos->y * multiplier);
+        std::string zstr = std::to_string(pos->z * multiplier);
+
+        xstr.erase(xstr.size() - (6 - decimalsToShow));
+        ystr.erase(ystr.size() - (6 - decimalsToShow));
+        zstr.erase(zstr.size() - (6 - decimalsToShow));
+
+        return StringMap {
+			{"x", xstr},
+			{"y", ystr},
+			{"z", zstr}
+        };
+    }
+
+    std::string formattedDimension(std::string dimension) {
+		std::string dimFormat = this->settings.getSettingByName<std::string>(dimension + "Format")->value;
+        return std::regex_replace(dimFormat, std::regex("\\{dim\\}"), dimension);
+    }
+
     void onRender(RenderEvent& event) {
         if (this->isEnabled() && SDK::clientInstance->getLocalPlayer() && SDK::getCurrentScreen() == "hud_screen") {
-            Vec3<float> *pos = SDK::clientInstance->getLocalPlayer()->getPosition();
-
             std::string coord = this->settings.getSettingByName<std::string>("text")->value;
+            std::string dimension = SDK::clientInstance->getBlockSource()->getDimension()->getName();
 
-            int decimalsToShow = this->settings.getSettingByName<bool>("showDecimals")->value ? std::floor(this->settings.getSettingByName<float>("decimalCount")->value) : -1;
+            if (dimension == "TheEnd") {
+                StringMap pos = getCoords(1.0f);
+                coord = std::regex_replace(coord, std::regex("\\{X\\}"), pos["x"]);
+                coord = std::regex_replace(coord, std::regex("\\{Y\\}"), pos["y"]);
+                coord = std::regex_replace(coord, std::regex("\\{Z\\}"), pos["z"]);
 
-            //std::ostringstream xStream, yStream, zStream;
-            //xStream << std::fixed << std::setprecision(0) << pos->x;
-            //yStream << std::fixed << std::setprecision(0) << pos->y;
-            //zStream << std::fixed << std::setprecision(0) << pos->z;
+                if (settings.getSettingByName<bool>("showDimName")->value) {
+                    coord = std::regex_replace(coord, std::regex("\\{D\\}"), formattedDimension("TheEnd"));
+                }
+                else {
+                    coord = std::regex_replace(coord, std::regex("\\{D\\}"), "");
+                }
 
-            std::string xstr = std::to_string(pos->x);
-            std::string ystr = std::to_string(pos->y);
-            std::string zstr = std::to_string(pos->z);
+                this->normalRenderCore(33, coord);
+            }
+            else {
+                std::string coord2 = coord;
 
-            xstr.erase(xstr.size() - (6 - decimalsToShow));
-            ystr.erase(ystr.size() - (6 - decimalsToShow));
-            zstr.erase(zstr.size() - (6 - decimalsToShow));
+                float otherDimMultiplier = 0.0f;
+                std::string otherDim = "";
+				if (dimension == "Overworld") {
+					otherDimMultiplier = 0.125f;
+                    otherDim = "Nether";
+				}
+				else if (dimension == "Nether") {
+					otherDimMultiplier = 8.0f;
+                    otherDim = "Overworld";
+				}
 
-            coord = std::regex_replace(coord, std::regex("\\{X\\}"), xstr);
-            coord = std::regex_replace(coord, std::regex("\\{Y\\}"), ystr);
-            coord = std::regex_replace(coord, std::regex("\\{Z\\}"), zstr);
+				StringMap defaultPos = getCoords(1.0f);
+				
+                coord = std::regex_replace(coord, std::regex("\\{X\\}"), defaultPos["x"]);
+                coord = std::regex_replace(coord, std::regex("\\{Y\\}"), defaultPos["y"]);
+                coord = std::regex_replace(coord, std::regex("\\{Z\\}"), defaultPos["z"]);
 
-            this->normalRenderCore(33, coord);
+                if (settings.getSettingByName<bool>("showDimName")->value) {
+                    coord = std::regex_replace(coord, std::regex("\\{D\\}"), formattedDimension(dimension));
+				}
+				else {
+					coord = std::regex_replace(coord, std::regex("\\{D\\}"), "");
+				}
+
+                std::string text = coord;
+
+				if (settings.getSettingByName<bool>("showOtherDimCoords")->value) {
+                    StringMap otherDimPos = getCoords(otherDimMultiplier);
+                    coord2 = std::regex_replace(coord2, std::regex("\\{X\\}"), otherDimPos["x"]);
+                    coord2 = std::regex_replace(coord2, std::regex("\\{Y\\}"), otherDimPos["y"]);
+                    coord2 = std::regex_replace(coord2, std::regex("\\{Z\\}"), otherDimPos["z"]);
+
+                    if (settings.getSettingByName<bool>("showDimName")->value) {
+                        coord2 = std::regex_replace(coord2, std::regex("\\{D\\}"), formattedDimension(otherDim));
+					}
+					else {
+						coord2 = std::regex_replace(coord2, std::regex("\\{D\\}"), "");
+					}
+
+					text = coord + "\n" + coord2;
+				}
+
+                this->normalRenderCore(33, text);
+            }
+
         }
     }
 
