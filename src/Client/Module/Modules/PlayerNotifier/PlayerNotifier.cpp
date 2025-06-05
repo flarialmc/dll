e3092cedd2330626167c081c9af17adbd71d1220
Melvin1663 <77887471+Melvin1663@users.noreply.github.com>
2025-06-06 04:42:50 +0800

    fix player notifier crash

--- src/Client/Module/Modules/PlayerNotifier/PlayerNotifier.cpp
+++ src/Client/Module/Modules/PlayerNotifier/PlayerNotifier.cpp
@@ -1,131 +1,139 @@
 #include "PlayerNotifier.hpp"
 
-PlayerNotifier::PlayerNotifier(): Module("Player Notifier", "Notifies you when a player is in the server.", IDR_CURSOR_PNG, "P")
+PlayerNotifier::PlayerNotifier() : Module("Player Notifier", "Notifies you when a player is in the server.", IDR_CURSOR_PNG, "P")
 {
-    Module::setup();
-    PlayerNotifier::defaultConfig();
-    PlayerNotifier::loadSettings();
+	Module::setup();
+	PlayerNotifier::defaultConfig();
+	PlayerNotifier::loadSettings();
 }
 
 void PlayerNotifier::defaultConfig()
 {
-    getKeybind();
-    Module::defaultConfig("core");
-    setDef("duration", 80.f);
-    setDef("player0", (std::string)"TheBarii");
-    setDef("player0Enabled", true);
-    setDef("keybind", (std::string)"P");
+	getKeybind();
+	Module::defaultConfig("core");
+	setDef("duration", 80.f);
+	setDef("player0", (std::string)"TheBarii");
+	setDef("player0Enabled", true);
+	setDef("keybind", (std::string)"P");
 }
 
 void PlayerNotifier::onEnable()
 {
-    Module::onEnable();
-    Listen(this, TickEvent, &PlayerNotifier::onTick);
-    Listen(this, KeyEvent, &PlayerNotifier::onKey)
+	Module::onEnable();
+	Listen(this, TickEvent, &PlayerNotifier::onTick);
+	Listen(this, KeyEvent, &PlayerNotifier::onKey)
 }
 
 void PlayerNotifier::onDisable()
 {
-    Module::onDisable();
-    Deafen(this, TickEvent, &PlayerNotifier::onTick);
-    Deafen(this, KeyEvent, &PlayerNotifier::onKey);
+	Module::onDisable();
+	Deafen(this, TickEvent, &PlayerNotifier::onTick);
+	Deafen(this, KeyEvent, &PlayerNotifier::onKey);
 }
 
 void PlayerNotifier::onSetup()
 {
-    keybindActions.clear();
-    keybindActions.push_back([this](std::vector<std::any> args) -> std::any {
-        check();
-        return {};
-    });
+	keybindActions.clear();
+	keybindActions.push_back([this](std::vector<std::any> args) -> std::any {
+		check();
+		return {};
+		});
 }
 
 void PlayerNotifier::loadSettings()
 {
-
-    Module::loadSettings();
-    for (const auto& settingPair : settings.settings) {
-        const std::string& name = settingPair.first;
-        if (name.find("player") != std::string::npos && name.find("Enabled") == std::string::npos) {
-            ++totalPlayers;
-        }
-    }
+	totalPlayers = 0;
+	Module::loadSettings();
+	for (const auto& settingPair : settings.settings) {
+		const std::string& name = settingPair.first;
+		if (name.contains("player") && name.contains("Enabled")) {
+			++totalPlayers;
+		}
+	}
 
 }
 
 void PlayerNotifier::check()
 {
-    std::unordered_map<mcUUID, PlayerListEntry>& playerMap = SDK::clientInstance->getLocalPlayer()->getLevel()->getPlayerMap();
+	std::unordered_map<mcUUID, PlayerListEntry>& playerMap = SDK::clientInstance->getLocalPlayer()->getLevel()->getPlayerMap();
 
-    for (const auto& [uuid, entry] : playerMap) {
-        for (int i = 0; i < totalPlayers; i++) {
-            if (!this->settings.getSettingByName<bool>("player" + FlarialGUI::cached_to_string(i) + "Enabled")) continue;
-            if (this->settings.getSettingByName<bool>("player" + FlarialGUI::cached_to_string(i) + "Enabled")->value) {
-                if (entry.name.find(this->settings.getSettingByName<std::string>("player" + FlarialGUI::cached_to_string(i))->value) != std::string::npos) {
-                    FlarialGUI::Notify(this->settings.getSettingByName<std::string>("player" + FlarialGUI::cached_to_string(i))->value + " is online!");
-                }
-            }
-        }
-    }
+	for (const auto& [uuid, entry] : playerMap) {
+		for (int i = 0; i < totalPlayers; i++) {
+			if (!this->settings.getSettingByName<bool>("player" + FlarialGUI::cached_to_string(i) + "Enabled")) continue;
+			if (this->settings.getSettingByName<bool>("player" + FlarialGUI::cached_to_string(i) + "Enabled")->value) {
+				if (entry.name.find(this->settings.getSettingByName<std::string>("player" + FlarialGUI::cached_to_string(i))->value) != std::string::npos) {
+					FlarialGUI::Notify(this->settings.getSettingByName<std::string>("player" + FlarialGUI::cached_to_string(i))->value + " is online!");
+				}
+			}
+		}
+	}
 }
 
 void PlayerNotifier::onTick(TickEvent& event)
 {
-    double intervalSeconds = getOps<float>("duration");
+	if (!this->isEnabled()) return;
+	double intervalSeconds = getOps<float>("duration");
 
-    auto now = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed = now - lastRun;
+	auto now = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsed = now - lastRun;
 
-    if ((elapsed.count() >= intervalSeconds || first) && SDK::getCurrentScreen() == "hud_screen") {
-        lastRun = now;
-        first = false;
+	if ((elapsed.count() >= intervalSeconds || first) && SDK::getCurrentScreen() == "hud_screen") {
+		lastRun = now;
+		first = false;
 
-        check();
+		check();
 
-    }
+	}
 }
 
 void PlayerNotifier::settingsRender(float settingsOffset)
 {
 
-    float x = Constraints::PercentageConstraint(0.019, "left");
-    float y = Constraints::PercentageConstraint(0.10, "top");
+	float x = Constraints::PercentageConstraint(0.019, "left");
+	float y = Constraints::PercentageConstraint(0.10, "top");
 
-    const float scrollviewWidth = Constraints::RelativeConstraint(0.12, "height", true);
+	const float scrollviewWidth = Constraints::RelativeConstraint(0.12, "height", true);
 
 
-    FlarialGUI::ScrollBar(x, y, 140, Constraints::SpacingConstraint(5.5, scrollviewWidth), 2);
-    FlarialGUI::SetScrollView(x - settingsOffset, Constraints::PercentageConstraint(0.00, "top"),
-                              Constraints::RelativeConstraint(1.0, "width"),
-                              Constraints::RelativeConstraint(0.88f, "height"));
+	FlarialGUI::ScrollBar(x, y, 140, Constraints::SpacingConstraint(5.5, scrollviewWidth), 2);
+	FlarialGUI::SetScrollView(x - settingsOffset, Constraints::PercentageConstraint(0.00, "top"),
+		Constraints::RelativeConstraint(1.0, "width"),
+		Constraints::RelativeConstraint(0.88f, "height"));
 
-    this->addHeader("Player Notifier");
-    this->addButton("Add new player", "", "ADD", [&]() {
-        this->settings.addSetting<std::string>("player" + FlarialGUI::cached_to_string(totalPlayers), "player" + FlarialGUI::cached_to_string(totalPlayers));
-        this->settings.addSetting<bool>("player" + FlarialGUI::cached_to_string(totalPlayers) + "Enabled", true);
-        totalPlayers++;
-    });
-    this->addSlider("Re-check", "(Seconds) After how long should it re-check for players", "duration", 500, 1, true);
-    this->addKeybind("Re-check Keybind", "Hold for 2 seconds!", "keybind", true);
-    this->extraPadding();
+	this->addHeader("Player Notifier");
+	this->addButton("Add new player", "", "ADD", [&]() {
+		this->settings.addSetting<std::string>("player" + FlarialGUI::cached_to_string(totalPlayers), "player" + FlarialGUI::cached_to_string(totalPlayers));
+		this->settings.addSetting<bool>("player" + FlarialGUI::cached_to_string(totalPlayers) + "Enabled", true);
+		totalPlayers++;
+		});
+	this->addSlider("Re-check", "(Seconds) After how long should it re-check for players", "duration", 500, 1, true);
+	this->addKeybind("Re-check Keybind", "Hold for 2 seconds!", "keybind", true);
+	this->extraPadding();
 
-    for (int i = 0; i < totalPlayers; i++) {
-        this->addHeader(this->settings.getSettingByName<std::string>("player" + FlarialGUI::cached_to_string(i))->value);
-        this->addToggle("Enabled", "Should it notify you?", "player" + FlarialGUI::cached_to_string(i));
-        this->addTextBox("Player Name", "", this->settings.getSettingByName<std::string>("player" + FlarialGUI::cached_to_string(i))->value);
-        this->extraPadding();
-    }
+	for (int i = 0; i < totalPlayers; i++) {
+		if (this->settings.getSettingByName<std::string>("player" + FlarialGUI::cached_to_string(i)) != nullptr) {
+			this->addHeader(this->settings.getSettingByName<std::string>("player" + FlarialGUI::cached_to_string(i))->value);
+			this->addToggle("Enabled", "Should it notify you?", this->settings.getSettingByName<bool>("player" + FlarialGUI::cached_to_string(i) + "Enabled")->value);
+			this->addTextBox("Player Name", "", this->settings.getSettingByName<std::string>("player" + FlarialGUI::cached_to_string(i))->value);
+			this->addButton("Remove Player", "", "Remove", [this, i]() {
+				this->settings.deleteSetting("player" + FlarialGUI::cached_to_string(i));
+				this->settings.deleteSetting("player" + FlarialGUI::cached_to_string(i) + "Enabled");
+				this->saveSettings();
+				});
+			this->extraPadding();
+		}
+	}
 
-    FlarialGUI::UnsetScrollView();
-    this->resetPadding();
+	FlarialGUI::UnsetScrollView();
+	this->resetPadding();
 }
 
 void PlayerNotifier::onKey(KeyEvent& event)
 {
-    if (this->isKeyPartOfKeybind(event.key)) {
-        if (this->isKeybind(event.keys)) { // key is defo pressed
-            keybindActions[0]({});
-        }
-    }
-
+	if (!this->isEnabled()) return;
+	if (this->isKeyPartOfKeybind(event.key)) {
+		if (this->isKeybind(event.keys)) { // key is defo pressed
+			keybindActions[0]({});
+		}
+	}
 }
