71dea783e04ad5ef6b9dde76484bb7c8c4bdf15e
prathfr <89528625+prathfr@users.noreply.github.com>
2025-06-11 20:16:12 +0530

    vertical coords and velocity

--- src/Client/Module/Modules/Coordinates/Coordinates.cpp
+++ src/Client/Module/Modules/Coordinates/Coordinates.cpp
@@ -19,13 +19,16 @@ void Coordinates::defaultConfig() {
     Module::defaultConfig("all");
     setDef("showDecimals", false);
     setDef("decimalCount", 2.f);
-    setDef("showOtherDimCoords", false);
+    setDef("verticalMode", false);
+    setDef("showVelocity", true);
+    setDef("showYvelocity", true);
     setDef("showDimName", false);
     setDef("useSameDimensionFormat", true);
     setDef("defaultDimFormat", (std::string) "{dim}");
     setDef("OverworldFormat", (std::string) "{dim}");
     setDef("NetherFormat", (std::string) "{dim}");
     setDef("TheEndFormat", (std::string) "{dim}");
+    setDef("showOtherDimCoords", false);
     if (ModuleManager::initialized) Client::SaveSettings();
 }
 
@@ -52,18 +55,21 @@ void Coordinates::settingsRender(float settingsOffset) {
     extraPadding();
 
     addHeader("Module Settings");
-    addToggle("Show Other Dimension Coords",
-              "Shows the other dimension\'s coordinates along with overworld coordinates.", "showOtherDimCoords");
-    addToggle("Show Dimension Name", "Shows the dimension name.", "showDimName");
-    addToggle("Use Same Dimension Format", "Uses the same format for all dimensions.", "useSameDimensionFormat");
-    addConditionalTextBox(getOps<bool>("useSameDimensionFormat"), "Dimension Format", "",
+    addToggle("Vertical Mode", "", "verticalMode");
+    addConditionalToggle(getOps<bool>("verticalMode"), "Show Velocity", "+ or -", "showVelocity");
+    addConditionalToggle(getOps<bool>("verticalMode") && getOps<bool>("showVelocity"), "Show Y-axis Velocity", "", "showYvelocity");
+    addConditionalToggle(!getOps<bool>("verticalMode"), "Show Dimension Name", "Shows the dimension name.", "showDimName");
+    addConditionalToggle(!getOps<bool>("verticalMode"), "Use Same Dimension Format", "Uses the same format for all dimensions.", "useSameDimensionFormat");
+    addConditionalTextBox(!getOps<bool>("verticalMode") && getOps<bool>("useSameDimensionFormat"), "Dimension Format", "",
                           getOps<std::string>("defaultDimFormat"));
-    addConditionalTextBox(!getOps<bool>("useSameDimensionFormat"), "Overworld Dimension Format", "",
+    addConditionalTextBox(!getOps<bool>("verticalMode") && !getOps<bool>("useSameDimensionFormat"), "Overworld Dimension Format", "",
                           getOps<std::string>("OverworldFormat"));
-    addConditionalTextBox(!getOps<bool>("useSameDimensionFormat"), "Nether Dimension Format", "",
+    addConditionalTextBox(!getOps<bool>("verticalMode") && !getOps<bool>("useSameDimensionFormat"), "Nether Dimension Format", "",
                           getOps<std::string>("NetherFormat"));
-    addConditionalTextBox(!getOps<bool>("useSameDimensionFormat"), "TheEnd Dimension Format", "",
+    addConditionalTextBox(!getOps<bool>("verticalMode") && !getOps<bool>("useSameDimensionFormat"), "TheEnd Dimension Format", "",
                           getOps<std::string>("TheEndFormat"));
+    addConditionalToggle(!getOps<bool>("verticalMode") && !getOps<bool>("verticalMode"), "Show Other Dimension Coords",
+              "Shows the other dimension\'s coordinates along with overworld coordinates.", "showOtherDimCoords");
     extraPadding();
 
     addHeader("Colors");
@@ -98,68 +104,95 @@ StringMap Coordinates::getCoords(float multiplier) {
 }
 
 std::string Coordinates::formattedDimension(std::string dimension) {
-    std::string dimFormat = this->settings.getSettingByName<std::string>(dimension + "Format")->value;
-    return std::regex_replace(dimFormat, std::regex("\\{dim\\}"), dimension);
+    if (getOps<bool>("useSameDimensionFormat")) return std::regex_replace(getOps<std::string>("defaultDimFormat"), std::regex("\\{dim\\}"), dimension);
+    else return std::regex_replace(getOps<std::string>(dimension + "Format"), std::regex("\\{dim\\}"), dimension);
+}
+
+std::string Coordinates::sigma(float val) {
+    if (val < 0) return "-";
+    else if (val > 0) return "+";
+    else return " ";
 }
 
 void Coordinates::onRender(RenderEvent &event) {
     if (this->isEnabled() && SDK::clientInstance->getLocalPlayer() && SDK::getCurrentScreen() == "hud_screen") {
-        std::string coord = getOps<std::string>("text");
-        std::string dimension = SDK::clientInstance->getBlockSource()->getDimension()->getName();
 
-        if (dimension == "TheEnd") {
-            StringMap pos = getCoords(1.0f);
-            coord = std::regex_replace(coord, std::regex("\\{X\\}"), pos["x"]);
-            coord = std::regex_replace(coord, std::regex("\\{Y\\}"), pos["y"]);
-            coord = std::regex_replace(coord, std::regex("\\{Z\\}"), pos["z"]);
+        if (!getOps<bool>("verticalMode")) {
+            std::string coord = getOps<std::string>("text");
+            std::string dimension = SDK::clientInstance->getBlockSource()->getDimension()->getName();
 
-            if (getOps<bool>("showDimName")) {
-                coord = std::regex_replace(coord, std::regex("\\{D\\}"), formattedDimension("TheEnd"));
-            } else {
-                coord = std::regex_replace(coord, std::regex("\\{D\\}"), "");
-            }
-
-            this->normalRenderCore(33, coord);
-        } else {
-            std::string coord2 = coord;
-
-            float otherDimMultiplier = 0.0f;
-            std::string otherDim = "";
-            if (dimension == "Overworld") {
-                otherDimMultiplier = 0.125f;
-                otherDim = "Nether";
-            } else if (dimension == "Nether") {
-                otherDimMultiplier = 8.0f;
-                otherDim = "Overworld";
-            }
-
-            StringMap defaultPos = getCoords(1.0f);
-
-            coord = std::regex_replace(coord, std::regex("\\{X\\}"), defaultPos["x"]);
-            coord = std::regex_replace(coord, std::regex("\\{Y\\}"), defaultPos["y"]);
-            coord = std::regex_replace(coord, std::regex("\\{Z\\}"), defaultPos["z"]);
-
-            if (getOps<bool>("showDimName")) {
-                coord = std::regex_replace(coord, std::regex("\\{D\\}"), formattedDimension(dimension));
-            } else {
-                coord = std::regex_replace(coord, std::regex("\\{D\\}"), "");
-            }
-
-            std::string text = coord;
-
-            if (getOps<bool>("showOtherDimCoords")) {
-                StringMap otherDimPos = getCoords(otherDimMultiplier);
-                coord2 = std::regex_replace(coord2, std::regex("\\{X\\}"), otherDimPos["x"]);
-                coord2 = std::regex_replace(coord2, std::regex("\\{Y\\}"), otherDimPos["y"]);
-                coord2 = std::regex_replace(coord2, std::regex("\\{Z\\}"), otherDimPos["z"]);
+            if (dimension == "TheEnd") {
+                StringMap pos = getCoords(1.0f);
+                coord = std::regex_replace(coord, std::regex("\\{X\\}"), pos["x"]);
+                coord = std::regex_replace(coord, std::regex("\\{Y\\}"), pos["y"]);
+                coord = std::regex_replace(coord, std::regex("\\{Z\\}"), pos["z"]);
 
                 if (getOps<bool>("showDimName")) {
-                    coord2 = std::regex_replace(coord2, std::regex("\\{D\\}"), formattedDimension(otherDim));
+                    coord = std::regex_replace(coord, std::regex("\\{D\\}"), formattedDimension("TheEnd"));
                 } else {
-                    coord2 = std::regex_replace(coord2, std::regex("\\{D\\}"), "");
+                    coord = std::regex_replace(coord, std::regex("\\{D\\}"), "");
                 }
 
-                text = coord + "\n" + coord2;
+                this->normalRenderCore(33, coord);
+            } else {
+                std::string coord2 = coord;
+
+                float otherDimMultiplier = 0.0f;
+                std::string otherDim = "";
+                if (dimension == "Overworld") {
+                    otherDimMultiplier = 0.125f;
+                    otherDim = "Nether";
+                } else if (dimension == "Nether") {
+                    otherDimMultiplier = 8.0f;
+                    otherDim = "Overworld";
+                }
+
+                StringMap defaultPos = getCoords(1.0f);
+
+                coord = std::regex_replace(coord, std::regex("\\{X\\}"), defaultPos["x"]);
+                coord = std::regex_replace(coord, std::regex("\\{Y\\}"), defaultPos["y"]);
+                coord = std::regex_replace(coord, std::regex("\\{Z\\}"), defaultPos["z"]);
+
+                if (getOps<bool>("showDimName")) {
+                    coord = std::regex_replace(coord, std::regex("\\{D\\}"), formattedDimension(dimension));
+                } else {
+                    coord = std::regex_replace(coord, std::regex("\\{D\\}"), "");
+                }
+
+                std::string text = coord;
+
+                if (getOps<bool>("showOtherDimCoords")) {
+                    StringMap otherDimPos = getCoords(otherDimMultiplier);
+                    coord2 = std::regex_replace(coord2, std::regex("\\{X\\}"), otherDimPos["x"]);
+                    coord2 = std::regex_replace(coord2, std::regex("\\{Y\\}"), otherDimPos["y"]);
+                    coord2 = std::regex_replace(coord2, std::regex("\\{Z\\}"), otherDimPos["z"]);
+
+                    if (getOps<bool>("showDimName")) {
+                        coord2 = std::regex_replace(coord2, std::regex("\\{D\\}"), formattedDimension(otherDim));
+                    } else {
+                        coord2 = std::regex_replace(coord2, std::regex("\\{D\\}"), "");
+                    }
+
+                    text = coord + "\n" + coord2;
+                }
+
+                this->normalRenderCore(33, text);
+            }
+        }
+        else {
+            StringMap pos = getCoords(1.f);
+
+            std::string text;
+
+            if (getOps<bool>("showVelocity")) {
+                Vec3<float> velo = SDK::clientInstance->getLocalPlayer()->getStateVectorComponent()->velocity;
+                text += std::format("{}X {}\n", sigma(velo.x), pos["x"]);
+                if (getOps<bool>("showYvelocity")) text += std::format("{}Y {}\n", sigma(velo.y), pos["y"]);
+                else text += std::format(" Y {}\n", pos["y"]);
+                text += std::format("{}Z {}", sigma(velo.z), pos["z"]);
+            }
+            else {
+                text += std::format("X {}\nY {}\nZ {}", pos["x"], pos["y"], pos["z"]);
             }
 
             this->normalRenderCore(33, text);
