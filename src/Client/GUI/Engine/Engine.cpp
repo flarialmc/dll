1c55399661988b0707de9851fa5887f40133d120
Melvin1663 <77887471+Melvin1663@users.noreply.github.com>
2025-06-13 15:04:10 +0800

    radio button cursor::hover

--- src/Client/GUI/Engine/Engine.cpp
+++ src/Client/GUI/Engine/Engine.cpp
@@ -69,8 +69,8 @@ std::unordered_map<int, ColorPicker> FlarialGUI::ColorPickers;
 std::unordered_map<int, DropdownStruct> FlarialGUI::DropDownMenus;
 std::unordered_map<int, KeybindSelector> FlarialGUI::KeybindSelectors;
 std::unordered_map<int, bool> FlarialGUI::ToggleIsHovering;
-std::unordered_map<int, bool> FlarialGUI::buttons;
 std::unordered_map<int, bool> FlarialGUI::buttonsHovered;
+std::unordered_map<int, bool> FlarialGUI::radioButtonsHovered;
 
 void FlarialGUI::OverrideAlphaValues(float percent) {
 	//FlarialGUI::lerp()
