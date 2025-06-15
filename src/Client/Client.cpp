d8609886ecbe40a3dd2427b109fa02236d083164
Bari <58800830+TheBarii@users.noreply.github.com>
2025-06-15 13:16:02 +0600

    beta: release

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -328,7 +328,7 @@ RECT previousRect = { 0 };
 bool InHudScreen = false;
 
 bool toes = false;
-
+//
 void Client::centerCursor() {
 	if (Client::disable) return;
 	if (hWnd != nullptr && Client::settings.getSettingByName<bool>("centreCursor")->value) {
