cf0c33633a0dc3a4d6251cbf1ba043c536a054ab
Melvin1663 <77887471+Melvin1663@users.noreply.github.com>
2025-07-10 22:28:30 +0800

    fix 3rdPnametage not showing in snaplook

--- src/Client/Module/Modules/ThirdPersonNametag/ThirdPerson.cpp
+++ src/Client/Module/Modules/ThirdPersonNametag/ThirdPerson.cpp
@@ -3,52 +3,42 @@
 #include "Client.hpp"
 
 ThirdPerson::ThirdPerson(): Module("Nametag", "Shows your nametag for you while\nin 3rd person mode.",
-                                   IDR_NAMETAG_PNG, "")
-{
-
+                                   IDR_NAMETAG_PNG, "") {
     address = GET_SIG_ADDRESS("ThirdPersonNametag");
 
     original.resize(6);
     Memory::copyBytes((LPVOID) address, original.data(), 6);
-
-    
 }
 
-void ThirdPerson::defaultConfig()
-{
+void ThirdPerson::defaultConfig() {
     Module::defaultConfig("core");
-    
 }
 
-void ThirdPerson::onEnable()
-{
+void ThirdPerson::onEnable() {
     patch();
     Listen(this, PerspectiveEvent, &ThirdPerson::onGetViewPerspective)
     Module::onEnable();
 }
 
-void ThirdPerson::onDisable()
-{
+void ThirdPerson::onDisable() {
     Deafen(this, PerspectiveEvent, &ThirdPerson::onGetViewPerspective)
     unpatch();
     Module::onDisable();
 }
 
-void ThirdPerson::patch()
-{
-    Memory::nopBytes((void *)address, 6);
+void ThirdPerson::patch() {
+    Memory::nopBytes((void *) address, 6);
     patched = true;
 }
 
-void ThirdPerson::unpatch()
-{
-    Memory::patchBytes((void *)address, original.data(), original.size());
+void ThirdPerson::unpatch() {
+    Memory::patchBytes((void *) address, original.data(), original.size());
     patched = false;
 }
 
-void ThirdPerson::onGetViewPerspective(PerspectiveEvent& event)
-{
-    if (event.getPerspective() == Perspective::FirstPerson and patched)  unpatch();
-    else if(event.getPerspective() != Perspective::FirstPerson and !patched) patch();
-
+void ThirdPerson::onGetViewPerspective(PerspectiveEvent &event) {
+    if (auto sl = ModuleManager::getModule("SnapLook"); sl && sl->active) {
+        if (!patched) patch();
+    } else if (event.getPerspective() == Perspective::FirstPerson && patched) unpatch();
+    else if (event.getPerspective() != Perspective::FirstPerson && !patched) patch();
 }
