5676f72d452ac690e6f7cea880eb4096d71a43e3
Melvin1663 <77887471+Melvin1663@users.noreply.github.com>
2025-07-13 10:01:20 +0800

    move guimessage class to a new file

--- src/SDK/Client/Render/GuiData.hpp
+++ src/SDK/Client/Render/GuiData.hpp
@@ -3,26 +3,7 @@
 #include <Utils/Utils.hpp>
 #include <Utils/Memory/Memory.hpp>
 #include <Utils/Memory/Game/SignatureAndOffsetManager.hpp>
-
-class GuiMessage {
-public: GuiMessage() = default;
-private:
-    char pad_0[8];
-public:
-    std::string msg;
-private:
-    char pad_1[40];
-public:
-    std::string ttsMsg;
-    std::string author;
-    std::string fullMsg;
-private:
-    char pad_2[40];
-public:
-    std::string xuid;
-private:
-    char pad_3[16];
-};
+#include "./GuiMessage.hpp"
 
 class GuiData {
 public:
