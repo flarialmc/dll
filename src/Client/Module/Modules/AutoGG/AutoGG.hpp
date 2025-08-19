a0bb6da550f60630e0645057b7600b76483c50a6
prathfr <89528625+prathfr@users.noreply.github.com>
2025-08-19 18:17:06 +0530

    fix soundtype enum

--- src/Client/Module/Modules/AutoGG/AutoGG.hpp
+++ src/Client/Module/Modules/AutoGG/AutoGG.hpp
@@ -10,9 +10,8 @@
 class AutoGG : public Module {
 
 public:
-	AutoGG() : Module("Auto GG", "Automatically sends a message when you win a game. Workes on\nThe Hive, Zeqa, CubeCraft, Lifeboat, Galaxite and Mineville. ",
+	AutoGG() : Module("Auto GG", "Automatically sends a message when you win a game. Workes on\nThe Hive, Zeqa, CubeCraft, Lifeboat, Galaxite and Mineville.",
 		IDR_LIKE_PNG, "") {
-		
 	};
 
 	void onEnable() override;
