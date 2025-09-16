fdcae057721b19b93e3b3054ac37fdfc739a9788
Bari <rifatbari28@gmail.com>
2025-09-16 18:51:15 -0400

    release: yeah just release that

--- src/Client/Hook/Hooks/Render/DirectX/DX12/SwapchainHook_DX12.cpp
+++ src/Client/Hook/Hooks/Render/DirectX/DX12/SwapchainHook_DX12.cpp
@@ -352,7 +352,7 @@ bool SwapchainHook::AllocateImageDescriptor(UINT imageId, D3D12_CPU_DESCRIPTOR_H
     }
 
     UINT descriptorIndex = (imageId - 100) + IMGUI_FONT_DESCRIPTORS;
-
+//
     if (descriptorIndex >= TOTAL_CONSOLIDATED_DESCRIPTORS) {
         Logger::custom(fg(fmt::color::red), "DescriptorAlloc", "ERROR: Image ID {} would exceed descriptor heap capacity (index {} >= {})",
                        imageId, descriptorIndex, TOTAL_CONSOLIDATED_DESCRIPTORS);
