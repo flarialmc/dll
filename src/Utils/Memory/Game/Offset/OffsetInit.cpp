3212e634e9a8c3d3934dd2ff0cffa07ec38b5bc4
Bari <rifatbari28@gmail.com>
2025-10-03 17:32:38 -0400

    camera offset

--- src/Utils/Memory/Game/Offset/OffsetInit.cpp
+++ src/Utils/Memory/Game/Offset/OffsetInit.cpp
@@ -15,6 +15,7 @@ void OffsetInit::init21110() {
     ADD_OFFSET("Attribute::Health", 6);
     ADD_OFFSET("Actor::baseTickVft", 29);
     ADD_OFFSET("ContainerScreenController::_handlePlaceAll", 57);
+    ADD_OFFSET("ClientInstance::camera", 0x288);
 
 }
 
