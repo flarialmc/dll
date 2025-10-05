c6f8d2e68290261285d055246489710e325ef8da
Bari <rifatbari28@gmail.com>
2025-10-05 18:10:07 -0400

    better item phy

--- src/SDK/Client/Render/Textures/Color.hpp
+++ src/SDK/Client/Render/Textures/Color.hpp
@@ -2,12 +2,12 @@
 
 namespace mce {
     class Color { // MCCColor
+
+    public:
         float r;
         float g;
         float b;
         float a;
-
-    public:
         Color(float r = 1.0f, float g = 1.0f, float b = 1.0f, float a = 1.0f) {
             this->r = r;
             this->g = g;
