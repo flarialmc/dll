3aaa92033aec863a76cafbca7cd671fdf7d79add
oAnshull <oanshul6969@gmail.com>
2025-10-07 21:13:38 +0530

    beta: build!!!!!!!!!!

--- src/Client/Hook/Hooks/Game/ChatScreenControllerHook.hpp
+++ src/Client/Hook/Hooks/Game/ChatScreenControllerHook.hpp
@@ -11,7 +11,7 @@ private:
         auto event = nes::make_holder<ChatScreenControllerTickEvent>(_this);
         eventMgr.trigger(event);
 
-        return funcOriginalTick(_this); // <----- Crashes right here!!!
+        return funcOriginalTick(_this); // <----- Crashes right here!!!!
     }
 
 public:
