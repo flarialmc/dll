8ac8d0d4df0f2c53cf8b0c46d4cd9a39f4ae9574
qixndev <yurogami48@gmail.com>
2026-03-29 02:54:16 +0800

    Update OpponentCPS.hpp

--- src/Client/Module/Modules/OpponentCPS/OpponentCPS.hpp
+++ src/Client/Module/Modules/OpponentCPS/OpponentCPS.hpp
@@ -1,1 +1,30 @@
+#pragma once
 
+#include "../HUDModule.hpp"
+#include "Events/Game/TickEvent.hpp"
+#include "Events/Input/KeyEvent.hpp"
+#include "Events/Render/RenderNameEvent.hpp"
+
+class OpponentCPS : public HUDModule {
+public:
+    OpponentCPS() : HUDModule(5, "Opponent CPS", "Ability to show your Opponent's Click per second.",
+        IDR_CPS_ICON_PNG, "O", {"cps", "opponent"}) {}
+
+    void onSetup() override;
+
+protected:
+    std::string getDisplayValue() override;
+
+    void customConfig() override;
+
+    void customSettings() override;
+
+    void customInit() override;
+
+    void customCleanup() override;
+
+private:
+    void onRenderName(RenderNameEvent& event);
+    
+    void onKey(KeyEvent& event);
+};
