2df2247898f2842a7e3362116ac1a2599b6f90b1
aaaaaaaaa <yurogami48@gmail.com>
2026-03-29 14:08:38 +0800

    Update ProjectileCounter.hpp

--- src/Client/Module/Modules/ProjectileCounter/ProjectileCounter.hpp
+++ src/Client/Module/Modules/ProjectileCounter/ProjectileCounter.hpp
@@ -1,1 +1,16 @@
+#pragma once
+#include "Module.hpp"
 
+class ProjectileCounter : public Module {
+public:
+    ProjectileCounter() : Module("Projectile Counter", "Shows how many projectiles you have.", Category::Visual) {}
+
+    void onEnable() override;
+    void onDisable() override;
+    void onRender(RenderEvent& event);
+    void settingsRender(float settingsOffset) override;
+    void defaultConfig() override;
+
+private:
+    int getItemCount(std::string itemName);
+};
